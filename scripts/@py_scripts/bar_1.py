75ed199f45dbd0adc7d06be226baca059e64ecc4
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2024-02-19 16:45:22 +0600

    XX

--- scripts/@py_scripts/bar_1.py
+++ scripts/@py_scripts/bar_1.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from datetime import datetime
 import subprocess
 import pyautogui
 import ctypes
@@ -24,6 +25,13 @@ def set_console_title(title):
 def close_window(event=None):
     ROOT.destroy()
 
+def create_button(text, command, bg_color, fg_color):
+    button = tk.Label(button_frame, text=text, bg=bg_color, fg=fg_color, height=0, width=0,
+                      relief="flat", highlightthickness=1, highlightbackground="#1d2027", padx=0, pady=0,
+                      font=("ink free", 10), cursor="hand2")
+    button.pack(side="top", padx=(0, 0))
+    button.bind("<Button-1>", command)
+
 set_console_title("1")
 # Create the main Tkinter window
 ROOT = tk.Tk()
@@ -40,37 +48,27 @@ def create_custom_border(parent):
 
 BORDER_FRAME = create_custom_border(ROOT)
 
-
-# # Calculate the screen width and height
+# Calculate the screen width and height
 screen_width = ROOT.winfo_screenwidth()
 screen_height = ROOT.winfo_screenheight()
 
 x = 0
 y = screen_height - 178
 
-ROOT.geometry(f"50x130+{x}+{y}") #! overall size of the window
-
+ROOT.geometry(f"50x130+{x}+{y}")  # Overall size of the window
 
 # Create a frame for the buttons
 button_frame = tk.Frame(ROOT, bg="#1d2027", width=1, height=0)
 button_frame.pack(side="top", padx=1, pady=10, fill="both")
 
-# Create the buttons
-buttons = [
-    {"text": "❌", "command": close_window},
-    {"text": "📏", "command": powertoys_ruler},
-    {"text": "🖱", "command": powertoys_mouse_crosshair},
-    {"text": "📝", "command": powertoys_TextExtract},
-    {"text": "♾️", "command": rclone_sync},
-    {"text": "💻", "command": windows_terminal},
-]
+# Define button texts, commands, background colors, and foreground colors
+button_texts = ["❌", "📏", "🖱", "📝", "♾️", "💻"]
+button_commands = [close_window, powertoys_ruler, powertoys_mouse_crosshair, powertoys_TextExtract, rclone_sync, windows_terminal]
+bg_colors = ["#1d2027", "#1d2027", "#1d2027", "#1d2027", "#1d2027", "#1d2027"]
+fg_colors = ["#FF0000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#3bda00", "#FFFFFF"]
 
-for button_info in buttons:
-    button = tk.Label(button_frame, text=button_info["text"], bg="#1d2027", fg="#FFFFFF", height=0, width=0,
-                      relief="flat", highlightthickness=1, highlightbackground="#1d2027", padx=0, pady=0,
-                      font=("ink free", 10), cursor="hand2")
-    button.pack(side="top", padx=(0, 0))
-    button.bind("<Button-1>", button_info["command"])
+# Create individual buttons
+for text, command, bg_color, fg_color in zip(button_texts, button_commands, bg_colors, fg_colors):
+    create_button(text, command, bg_color, fg_color)
 
 ROOT.mainloop()
-
