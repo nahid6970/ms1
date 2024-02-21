c09b6c104d83fd8b09fa73c8898437ea672355e9
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2024-02-22 02:49:53 +0600

    XX

--- mypygui.py
+++ mypygui.py
@@ -370,7 +370,7 @@ def update_status():
 def extra_bar(event=None):
     subprocess.Popen(["powershell", "start-process", "D:\\@git\\ms1\\scripts\\@py_scripts\\bar_1.py", "-WindowStyle", "Hidden"])
 
-BOX_ROW_ROOT = tk.Frame(ROOT, bg="#1d2027") ; BOX_ROW_ROOT.pack(side="top", anchor="e", pady=(4,3),padx=(5,3))
+BOX_ROW_ROOT = tk.Frame(ROOT, bg="#1d2027") ; BOX_ROW_ROOT.pack(side="top", anchor="e", pady=(5,3),padx=(5,3))
 
 def create_label1(
                   parent,
