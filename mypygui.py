03a5cf65e304cdc7c998f5bd7ed8646d97e6630f
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2024-03-01 20:58:48 +0600

    XX

--- mypygui.py
+++ mypygui.py
@@ -1280,8 +1280,10 @@ def show_options(options):
     x = (screen_width - 300) // 2
     y = (screen_height - 100) // 2
     top.geometry(f"300x100+{x}+{y}")
+
     frame = tk.Frame(top, bg="#1d2027") 
     frame.pack(side="top", expand=True, fill="none", anchor="center")
+    
     for option in options:
         btn = tk.Button(frame, text=option["text"], command=option["command"], foreground="#fff", background="#1d2027", padx=10, pady=5, borderwidth=2, relief="raised")
         btn.pack(side="left", padx=5, pady=5, anchor="center")
@@ -1298,12 +1300,20 @@ def show_options(options):
 chkbx_rclone = tk.IntVar()
 chkbx_rufus = tk.IntVar()
 chkbx_ruffle = tk.IntVar()
+chkbx_powertoys = tk.IntVar()
+chkbx_rssguard = tk.IntVar()
+chkbx_ack = tk.IntVar()
+chkbx_adb = tk.IntVar()
 
 # Define applications and their information
 applications = [
-    {"name": "Rclone", "scoop_name": "rclone"        , "scoop_path": r'C:\Users\nahid\scoop\apps\rclone\current\rclone.exe', "winget_name": "Rclone.Rclone", "winget_path": r'C:\Users\nahid\AppData\Local\Microsoft\WinGet\Packages\Rclone.Rclone_Microsoft.Winget.Source_8wekyb3d8bbwe\rclone-v1.65.2-windows-amd64\rclone.exe', "chkbx_var": chkbx_rclone},
-    {"name": "Rufus" , "scoop_name": "rufus"         , "scoop_path": r'C:\Users\nahid\scoop\apps\rufus\current\rufus.exe', "winget_name": "Rufus.Rufus"  , "winget_path": r"C:\Users\nahid\AppData\Local\Microsoft\WinGet\Links\rufus.exe", "chkbx_var": chkbx_rufus} ,
-    {"name": "Ruffle", "scoop_name": "ruffle-nightly", "scoop_path": r'C:\Users\nahid\scoop\apps\ruffle-nightly\current\ruffle.exe', "winget_name": "", "winget_path": "xx", "chkbx_var": chkbx_ruffle},
+    {"name": "Rclone"   , "scoop_name": "rclone"        , "scoop_path": r'C:\Users\nahid\scoop\apps\rclone\current\rclone.exe'          , "winget_name": "Rclone.Rclone"        , "winget_path": r'C:\Users\nahid\AppData\Local\Microsoft\WinGet\Packages\Rclone.Rclone_Microsoft.Winget.Source_8wekyb3d8bbwe\rclone-v1.65.2-windows-amd64\rclone.exe', "chkbx_var": chkbx_rclone}   ,
+    {"name": "Rufus"    , "scoop_name": "rufus"         , "scoop_path": r'C:\Users\nahid\scoop\apps\rufus\current\rufus.exe'            , "winget_name": "Rufus.Rufus"          , "winget_path": r"C:\Users\nahid\AppData\Local\Microsoft\WinGet\Links\rufus.exe"                                                                                     , "chkbx_var": chkbx_rufus}    ,
+    {"name": "Ruffle"   , "scoop_name": "ruffle-nightly", "scoop_path": r'C:\Users\nahid\scoop\apps\ruffle-nightly\current\ruffle.exe'  , "winget_name": ""                     , "winget_path": "xx"                                                                                                                                                 , "chkbx_var": chkbx_ruffle}   ,
+    {"name": "PowerToys", "scoop_name": "powertoys"     , "scoop_path": r'C:\Users\nahid\scoop\apps\PowerToys\current\PowerToys.exe'    , "winget_name": "Microsoft.PowerToys"  , "winget_path": "xx"                                                                                                                                                 , "chkbx_var": chkbx_powertoys},
+    {"name": "Rss Guard", "scoop_name": "rssguard"      , "scoop_path": r'C:\Users\nahid\scoop\apps\rssguard\current\rssguard.exe'      , "winget_name": "martinrotter.RSSGuard", "winget_path": "xx"                                                                                                                                                 , "chkbx_var": chkbx_rssguard} ,
+    {"name": "ack"      , "scoop_name": "ack"           , "scoop_path": r'C:\Users\nahid\scoop\apps\ack\current\ack.bat'                , "winget_name": ""                     , "winget_path": "xx"                                                                                                                                                 , "chkbx_var": chkbx_ack}      ,
+    {"name": "adb"      , "scoop_name": "adb"           , "scoop_path": r'C:\Users\nahid\scoop\apps\adb\current\platform-tools\adb.exe' , "winget_name": ""                     , "winget_path": "xx"                                                                                                                                                 , "chkbx_var": chkbx_adb}      ,
     # Add more applications here
 ]
 # Create and pack checkboxes, check buttons, install buttons, and uninstall buttons for each application
