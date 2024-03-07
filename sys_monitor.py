af12720b5be8e96c80916e4f513c50eb0ff38c4d
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2024-03-07 15:50:44 +0600

    XX

--- sys_monitor.py
+++ sys_monitor.py
@@ -247,8 +247,8 @@ label_properties = [
     (BOX_ROW_ROOT,"#1d2027","#ff0000","2","1","flat",1,0,"e", 0,"#FFFFFF", ("agency" , 12, "bold"), 1, 8,2, "X") ,
     (BOX_ROW_ROOT,"#000000","#FFFFFF","1","1","flat",0,0,"e", 1,"#FFFFFF", ("agency" , 10, "bold"), 1, 7,2, "+") ,
     (BOX_ROW_ROOT,"#1d2027","#00FF00","2","1","flat",1,0,"e", 0,"#FFFFFF", ("agency" , 10, "bold"), 1, 5,2, "S") ,
-    (BOX_ROW_ROOT,"#1d2027","#FFFFFF","0","1","flat",1,0,"e", 0,"#FFFFFF", ("agency" , 10, "bold"), 1, 6,1, "m") ,
-    (BOX_ROW_ROOT,"#1d2027","#FFFFFF","0","1","flat",1,0,"e", 0,"#FFFFFF", ("agency" , 10, "bold"), 2,6 ,1, "m") ,
+    (BOX_ROW_ROOT,"#1d2027","#FFFFFF","3","1","flat",1,0,"e", 0,"#FFFFFF", ("agency" , 10, "bold"), 1, 6,1, "m") ,
+    (BOX_ROW_ROOT,"#1d2027","#FFFFFF","3","1","flat",1,0,"e", 0,"#FFFFFF", ("agency" , 10, "bold"), 2,6 ,1, "m") ,
 ]
 labels = [create_label1(*prop) for prop in label_properties]
 LB_XXX, LB_1, bkup, STATUS_MS1, STATUS_MS2 = labels
