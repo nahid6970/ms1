8fadb021a76f90ad451005ad7fadf3428956e8b0
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2024-03-09 15:25:55 +0600

    XX

--- mypygui.py
+++ mypygui.py
@@ -367,30 +367,30 @@ def clear_screen():
 BOX_ROW_ROOT = tk.Frame(ROOT, bg="#1d2027")
 BOX_ROW_ROOT.pack(side="right", anchor="ne", pady=(2,2),padx=(3,1))
 
-def create_label( text, parent, bg_color, fg_color, width, height, relief, font, padx, pady, anchor, ht, htc, row, column, rowspan, columnspan ):
+def create_label( text, parent, bg_color, fg_color, width, height, relief, font,   ht, htc, padx, pady,anchor,row, column, rowspan, columnspan ):
     label = tk.Label( parent, text=text, bg=bg_color, fg=fg_color, width=width, height=height, relief=relief, font=font, highlightthickness=ht, highlightbackground=htc )
     label.grid(row=row, column=column, padx=padx, pady=pady, sticky=anchor, rowspan=rowspan,columnspan=columnspan)
     return label
 
 label_properties = [
-("CPU"   ,BOX_ROW_ROOT,"#1d2027","#ffffff","4","1","flat",("JetBrainsMonoNF",10,"bold"),(0 ,0),(0,0),"w",0,"#FFFFFF",1,1,1,1),
-("GPU"   ,BOX_ROW_ROOT,"#1d2027","#ffffff","4","1","flat",("JetBrainsMonoNF",10,"bold"),(0 ,0),(0,0),"w",0,"#FFFFFF",1,2,1,1),
-("RAM"   ,BOX_ROW_ROOT,"#1d2027","#ffffff","4","1","flat",("JetBrainsMonoNF",10,"bold"),(0 ,0),(0,0),"w",0,"#FFFFFF",1,3,1,1),
-("Disk_C",BOX_ROW_ROOT,"#1d2027","#ffffff","4","1","flat",("JetBrainsMonoNF",10,"bold"),(0 ,0),(0,0),"w",0,"#FFFFFF",1,4,1,1),
-("Disk_D",BOX_ROW_ROOT,"#1d2027","#ffffff","4","1","flat",("JetBrainsMonoNF",10,"bold"),(0 ,0),(0,0),"w",0,"#FFFFFF",1,5,1,1),
-("^"     ,BOX_ROW_ROOT,"#1d2027","#ffffff","5","1","flat",("JetBrainsMonoNF",10,"bold"),(3 ,0),(0,0),"w",0,"#FFFFFF",1,6,1,1),
-("v"     ,BOX_ROW_ROOT,"#1d2027","#ffffff","5","1","flat",("JetBrainsMonoNF",10,"bold"),(3 ,0),(0,0),"w",0,"#FFFFFF",1,7,1,1),
+("CPU"   ,BOX_ROW_ROOT,"#1d2027","#ffffff","4","1","flat",("JetBrainsMonoNF",10,"bold"),0,"#FFFFFF",(0 ,0),(0,0),"w",1,1 ,1,1),
+("GPU"   ,BOX_ROW_ROOT,"#1d2027","#ffffff","4","1","flat",("JetBrainsMonoNF",10,"bold"),0,"#FFFFFF",(0 ,0),(0,0),"w",1,2 ,1,1),
+("RAM"   ,BOX_ROW_ROOT,"#1d2027","#ffffff","4","1","flat",("JetBrainsMonoNF",10,"bold"),0,"#FFFFFF",(0 ,0),(0,0),"w",1,3 ,1,1),
+("Disk_C",BOX_ROW_ROOT,"#1d2027","#ffffff","4","1","flat",("JetBrainsMonoNF",10,"bold"),0,"#FFFFFF",(0 ,0),(0,0),"w",1,4 ,1,1),
+("Disk_D",BOX_ROW_ROOT,"#1d2027","#ffffff","4","1","flat",("JetBrainsMonoNF",10,"bold"),0,"#FFFFFF",(0 ,0),(0,0),"w",1,5 ,1,1),
+("^"     ,BOX_ROW_ROOT,"#1d2027","#ffffff","5","1","flat",("JetBrainsMonoNF",10,"bold"),0,"#FFFFFF",(3 ,0),(0,0),"w",1,6 ,1,1),
+("v"     ,BOX_ROW_ROOT,"#1d2027","#ffffff","5","1","flat",("JetBrainsMonoNF",10,"bold"),0,"#FFFFFF",(3 ,0),(0,0),"w",1,7 ,1,1),
 
 
-("Git"   ,BOX_ROW_ROOT,"#1d2027","#009fff","0","0","flat",("JetBrainsMonoNF",8 ,"bold"),(10,0),(0,0),"w",0,"#FFFFFF",1,14,2,1),
-("m"     ,BOX_ROW_ROOT,"#1d2027","#FFFFFF","0","0","flat",("JetBrainsMonoNF",10,"bold"),(0 ,0),(0,0),"w",0,"#FFFFFF",1,15,1,1),
-("m"     ,BOX_ROW_ROOT,"#1d2027","#FFFFFF","0","0","flat",("JetBrainsMonoNF",10,"bold"),(0 ,0),(0,0),"w",0,"#FFFFFF",1,16,1,1),
-("+"     ,BOX_ROW_ROOT,"#000000","#FFFFFF","0","0","flat",("JetBrainsMonoNF",10,"bold"),(10,0),(0,0),"w",0,"#FFFFFF",1,13,2,1),
-("P"     ,BOX_ROW_ROOT,"#000000","#FFFFFF","0","0","flat",("JetBrainsMonoNF",10,"bold"),(0 ,0),(0,0),"w",0,"#FFFFFF",1,12,1,1),
-("<"     ,BOX_ROW_ROOT,"#1d2027","#FFFFFF","0","0","flat",("JetBrainsMonoNF",10,"bold"),(0 ,0),(0,0),"w",0,"#FFFFFF",1,11,1,1),
-("v"     ,BOX_ROW_ROOT,"#1d2027","#FFFFFF","0","0","flat",("JetBrainsMonoNF",10,"bold"),(0 ,0),(0,0),"w",0,"#FFFFFF",1,10,1,1),
-("M"     ,BOX_ROW_ROOT,"#1d2027","#26b2f3","0","0","flat",("JetBrainsMonoNF",10,"bold"),(0 ,0),(0,0),"w",0,"#FFFFFF",1,9,1,1),
-("X"     ,BOX_ROW_ROOT,"#1d2027","#ff0000","0","0","flat",("JetBrainsMonoNF",12,"bold"),(0 ,0),(0,0),"w",0,"#FFFFFF",1,8,2,1),
+("Git"   ,BOX_ROW_ROOT,"#1d2027","#009fff","0","0","flat",("JetBrainsMonoNF",8 ,"bold"),0,"#FFFFFF",(10,0),(0,0),"w",1,8 ,2,1),
+("m"     ,BOX_ROW_ROOT,"#1d2027","#FFFFFF","0","0","flat",("JetBrainsMonoNF",10,"bold"),0,"#FFFFFF",(0 ,0),(0,0),"w",1,9 ,1,1),
+("m"     ,BOX_ROW_ROOT,"#1d2027","#FFFFFF","0","0","flat",("JetBrainsMonoNF",10,"bold"),0,"#FFFFFF",(0 ,0),(0,0),"w",1,10,1,1),
+("+"     ,BOX_ROW_ROOT,"#000000","#FFFFFF","0","0","flat",("JetBrainsMonoNF",10,"bold"),0,"#FFFFFF",(10,0),(0,0),"w",1,11,2,1),
+("P"     ,BOX_ROW_ROOT,"#000000","#FFFFFF","0","0","flat",("JetBrainsMonoNF",10,"bold"),0,"#FFFFFF",(0 ,0),(0,0),"w",1,12,1,1),
+("<"     ,BOX_ROW_ROOT,"#1d2027","#FFFFFF","0","0","flat",("JetBrainsMonoNF",10,"bold"),0,"#FFFFFF",(0 ,0),(0,0),"w",1,13,1,1),
+("v"     ,BOX_ROW_ROOT,"#1d2027","#FFFFFF","0","0","flat",("JetBrainsMonoNF",10,"bold"),0,"#FFFFFF",(0 ,0),(0,0),"w",1,14,1,1),
+("M"     ,BOX_ROW_ROOT,"#1d2027","#26b2f3","0","0","flat",("JetBrainsMonoNF",10,"bold"),0,"#FFFFFF",(0 ,0),(0,0),"w",1,15,1,1),
+("X"     ,BOX_ROW_ROOT,"#1d2027","#ff0000","0","0","flat",("JetBrainsMonoNF",12,"bold"),0,"#FFFFFF",(0 ,0),(0,0),"w",1,16,2,1),
 ]
 labels = [create_label(*prop) for prop in label_properties]
 LB_CPU, LB_GPU, LB_RAM, LB_DUC, LB_DUD, LB_UPLOAD, LB_DWLOAD, bkup, STATUS_MS1, STATUS_MS2,LB_1,BT_TOPMOST,LB_S,LB_L,LB_M,LB_XXX = labels
