5116f2465c7552d7bf4fe6825617f37ce087a412
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2024-03-09 13:06:19 +0600

    XX

--- mypygui.py
+++ mypygui.py
@@ -373,39 +373,39 @@ def extra_bar(event=None):
 BOX_ROW_ROOT = tk.Frame(ROOT, bg="#1d2027")
 BOX_ROW_ROOT.pack(side="right", anchor="ne", pady=(2,2),padx=(3,1))
 
-def create_label2( text, parent, bg_color, fg_color, width, height, relief, font, padx, pady, anchor, ht, htc, row, column ):
+def create_label2( text, parent, bg_color, fg_color, width, height, relief, font, padx, pady, anchor, ht, htc, row, column, rowspan, columnspan ):
     label = tk.Label( parent, text=text, bg=bg_color, fg=fg_color, width=width, height=height, relief=relief, font=font, highlightthickness=ht, highlightbackground=htc )
-    label.grid(row=row, column=column, padx=padx, pady=pady, sticky=anchor)
+    label.grid(row=row, column=column, padx=padx, pady=pady, sticky=anchor, rowspan=rowspan,columnspan=columnspan)
     return label
 
 label_properties = [
-("CPU"   ,BOX_ROW_ROOT,"#1d2027","#ffffff","4","1","flat",("arial",10,"bold"),(0,0),(0,0),"w",0,"#FFFFFF",1,1)     ,
-("GPU"   ,BOX_ROW_ROOT,"#1d2027","#ffffff","4","1","flat",("arial",10,"bold"),(0,0),(0,0),"w",0,"#FFFFFF",2,1)     ,
-("RAM"   ,BOX_ROW_ROOT,"#1d2027","#ffffff","4","1","flat",("arial",10,"bold"),(0,0),(0,0),"w",0,"#FFFFFF",1,2)     ,
-("Disk C",BOX_ROW_ROOT,"#1d2027","#ffffff","4","1","flat",("arial",10,"bold"),(0,0),(0,0),"w",0,"#FFFFFF",1,3)     ,
-("Disk D",BOX_ROW_ROOT,"#1d2027","#ffffff","4","1","flat",("arial",10,"bold"),(0,0),(0,0),"w",0,"#FFFFFF",2,3)     ,
-("^"     ,BOX_ROW_ROOT,"#1d2027","#ffffff","5","1","flat",("arial",10,"bold"),(3,0),(0,0),"w",0,"#FFFFFF",1,4)     ,
-("v"     ,BOX_ROW_ROOT,"#1d2027","#ffffff","5","1","flat",("arial",10,"bold"),(3,0),(0,0),"w",0,"#FFFFFF",2,4)     ,
+("CPU"   ,BOX_ROW_ROOT,"#1d2027","#ffffff","4","1","flat",("arial",10,"bold"),(0,0),(0,0),"w",0,"#FFFFFF",1,1,1,1)     ,
+("GPU"   ,BOX_ROW_ROOT,"#1d2027","#ffffff","4","1","flat",("arial",10,"bold"),(0,0),(0,0),"w",0,"#FFFFFF",2,1,1,1)     ,
+("RAM"   ,BOX_ROW_ROOT,"#1d2027","#ffffff","4","1","flat",("arial",10,"bold"),(0,0),(0,0),"w",0,"#FFFFFF",1,2,1,1)     ,
+("Disk C",BOX_ROW_ROOT,"#1d2027","#ffffff","4","1","flat",("arial",10,"bold"),(0,0),(0,0),"w",0,"#FFFFFF",1,3,1,1)     ,
+("Disk D",BOX_ROW_ROOT,"#1d2027","#ffffff","4","1","flat",("arial",10,"bold"),(0,0),(0,0),"w",0,"#FFFFFF",2,3,1,1)     ,
+("^"     ,BOX_ROW_ROOT,"#1d2027","#ffffff","5","1","flat",("arial",10,"bold"),(3,0),(0,0),"w",0,"#FFFFFF",1,4,1,1)     ,
+("v"     ,BOX_ROW_ROOT,"#1d2027","#ffffff","5","1","flat",("arial",10,"bold"),(3,0),(0,0),"w",0,"#FFFFFF",2,4,1,1)     ,
 ]
 
 labels = [create_label2(*prop) for prop in label_properties]
 LB_CPU, LB_GPU, LB_RAM, LB_DUC, LB_DUD, LB_UPLOAD, LB_DWLOAD = labels
 
-def create_label1(text,parent, bg_color, fg_color, width, height, relief, padx, pady, anchor, ht, htc, font, row, column,rowspan):
+def create_label1(text,parent, bg_color, fg_color, width, height, relief, padx, pady, anchor, ht, htc, font, row, column,rowspan,columnspan):
     label = tk.Label(parent, text=text, bg=bg_color, fg=fg_color, width=width, height=height, relief=relief, font=font, highlightthickness=ht, highlightcolor=htc)
-    label.grid(row=row, column=column, padx=padx, pady=pady, sticky=anchor, rowspan=rowspan)
+    label.grid(row=row, column=column, padx=padx, pady=pady, sticky=anchor, rowspan=rowspan,columnspan=columnspan)
     return label
 
 label_properties = [
-("X"  ,BOX_ROW_ROOT,"#1d2027","#ff0000","0","0","flat",(0 ,0),(0,0),"w",0,"#FFFFFF",("JetBrainsMono NF"   ,12,"bold"),3,8  ,2)   ,
-("M"  ,BOX_ROW_ROOT,"#1d2027","#26b2f3","0","0","flat",(0 ,0),(0,0),"w",0,"#FFFFFF",("JetBrainsMono NF"   ,10,"bold"),3,2  ,1)   ,
-("v"  ,BOX_ROW_ROOT,"#1d2027","#FFFFFF","0","0","flat",(0 ,0),(0,0),"w",0,"#FFFFFF",("JetBrainsMono NF"   ,10,"bold"),3,3  ,1)   ,
-("<"  ,BOX_ROW_ROOT,"#1d2027","#FFFFFF","0","0","flat",(0 ,0),(0,0),"w",0,"#FFFFFF",("JetBrainsMono NF"   ,10,"bold"),3,4  ,1)   ,
-("P"  ,BOX_ROW_ROOT,"#000000","#FFFFFF","0","0","flat",(0 ,0),(0,0),"w",0,"#FFFFFF",("JetBrainsMono NF"   ,10,"bold"),3,5  ,1)   ,
-("+"  ,BOX_ROW_ROOT,"#000000","#FFFFFF","0","0","flat",(10,0),(0,0),"w",0,"#FFFFFF",("JetBrainsMono NF"   ,10,"bold"),3,8  ,2)   ,
-("Git",BOX_ROW_ROOT,"#1d2027","#009fff","0","0","flat",(10,0),(0,0),"w",0,"#FFFFFF",("JetBrainsMono NF"   ,8 ,"bold"),3,9  ,2)   ,
-("m"  ,BOX_ROW_ROOT,"#1d2027","#FFFFFF","0","0","flat",(0 ,0),(0,0),"w",0,"#FFFFFF",("JetBrainsMono NF"   ,10,"bold"),3,10 ,1)   ,
-("m"  ,BOX_ROW_ROOT,"#1d2027","#FFFFFF","0","0","flat",(0 ,0),(0,0),"w",0,"#FFFFFF",("JetBrainsMono NF"   ,10,"bold"),3,11 ,1)   ,
+("X"  ,BOX_ROW_ROOT,"#1d2027","#ff0000","0","0","flat",(0 ,0),(0,0),"w",0,"#FFFFFF",("JetBrainsMono NF"   ,12,"bold"),3,8  ,2,1)   ,
+("M"  ,BOX_ROW_ROOT,"#1d2027","#26b2f3","0","0","flat",(0 ,0),(0,0),"w",0,"#FFFFFF",("JetBrainsMono NF"   ,10,"bold"),3,2  ,1,1)   ,
+("v"  ,BOX_ROW_ROOT,"#1d2027","#FFFFFF","0","0","flat",(0 ,0),(0,0),"w",0,"#FFFFFF",("JetBrainsMono NF"   ,10,"bold"),3,3  ,1,1)   ,
+("<"  ,BOX_ROW_ROOT,"#1d2027","#FFFFFF","0","0","flat",(0 ,0),(0,0),"w",0,"#FFFFFF",("JetBrainsMono NF"   ,10,"bold"),3,4  ,1,1)   ,
+("P"  ,BOX_ROW_ROOT,"#000000","#FFFFFF","0","0","flat",(0 ,0),(0,0),"w",0,"#FFFFFF",("JetBrainsMono NF"   ,10,"bold"),3,5  ,1,1)   ,
+("+"  ,BOX_ROW_ROOT,"#000000","#FFFFFF","0","0","flat",(10,0),(0,0),"w",0,"#FFFFFF",("JetBrainsMono NF"   ,10,"bold"),3,8  ,2,1)   ,
+("Git",BOX_ROW_ROOT,"#1d2027","#009fff","0","0","flat",(10,0),(0,0),"w",0,"#FFFFFF",("JetBrainsMono NF"   ,8 ,"bold"),3,9  ,2,1)   ,
+("m"  ,BOX_ROW_ROOT,"#1d2027","#FFFFFF","0","0","flat",(0 ,0),(0,0),"w",0,"#FFFFFF",("JetBrainsMono NF"   ,10,"bold"),3,10 ,1,1)   ,
+("m"  ,BOX_ROW_ROOT,"#1d2027","#FFFFFF","0","0","flat",(0 ,0),(0,0),"w",0,"#FFFFFF",("JetBrainsMono NF"   ,10,"bold"),3,11 ,1,1)   ,
 ]
 labels = [create_label1(*prop) for prop in label_properties]
 LB_XXX,LB_M,LB_L,LB_S,BT_TOPMOST, LB_1, bkup, STATUS_MS1, STATUS_MS2 = labels
