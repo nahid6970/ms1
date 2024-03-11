851cad72c6f0dca5f73c00b63918e883847df534
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2024-03-11 13:21:57 +0600

    XX

--- mypygui.py
+++ mypygui.py
@@ -1432,10 +1432,10 @@ def create_button(parent, text="", image=None, compound=None, command=None, heig
 
 # Button Properties
 button_properties = [
-    {"parent": BOX_1, "image": shutdown_window, "compound": tk.TOP, "text": "", "command": force_shutdown, "row": 1, "column": 1},
-    {"parent": BOX_1, "image": restart_window, "compound": tk.TOP, "text": "", "command": force_restart, "row": 1, "column": 2},
-    {"parent": BOX_1, "image": backup_image, "compound": tk.TOP, "text": "", "command": open_backup, "row": 1, "column": 3},
-    {"parent": BOX_1, "image": update_image, "compound": tk.TOP, "text": "", "command": open_update, "row": 1, "column": 4}
+    {"parent": BOX_1,"image": shutdown_window,"compound": tk.TOP,"text": "","command": force_shutdown,"row": 1,"column": 1, "rowspan":1, "columnspan":1},
+    {"parent": BOX_1,"image": restart_window ,"compound": tk.TOP,"text": "","command": force_restart ,"row": 1,"column": 2, "rowspan":1, "columnspan":1},
+    {"parent": BOX_1,"image": backup_image   ,"compound": tk.TOP,"text": "","command": open_backup   ,"row": 1,"column": 3, "rowspan":1, "columnspan":1},
+    {"parent": BOX_1,"image": update_image   ,"compound": tk.TOP,"text": "","command": open_update   ,"row": 1,"column": 4, "rowspan":1, "columnspan":1},
 ]
 
 # Create Buttons
