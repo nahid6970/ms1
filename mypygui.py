ef4dbdd1f93076df5311f7f01d97d0a175a11099
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2024-03-12 11:28:34 +0600

    XX

--- mypygui.py
+++ mypygui.py
@@ -1442,7 +1442,75 @@ buttons = [create_button(**prop) for prop in button_properties]
 
 
 
+BOX_2 = tk.Frame(MAIN_FRAME, bg="#1d2027")
+BOX_2.pack(pady=(5,0))
 
+class RoundedButton(tk.Canvas):
+
+    def __init__(self, master=None, text:str="", radius=25, btnforeground="#000000", btnbackground="#ffffff", clicked=None, width=None, height=None, *args, **kwargs):
+        super(RoundedButton, self).__init__(master, *args, **kwargs)
+        self.config(bg=self.master["bg"])
+        self.btnbackground = btnbackground
+        self.clicked = clicked
+
+        self.radius = radius
+
+        self.circle = self.create_oval(0, 0, 0, 0, tags="button", fill=btnbackground)
+        self.text = self.create_text(0, 0, text=text, tags="button", fill=btnforeground, font=("Times", 30), justify="center")
+
+        self.tag_bind("button", "<ButtonPress>", self.border)
+        self.tag_bind("button", "<ButtonRelease>", self.border)
+        self.bind("<Configure>", self.resize)
+
+        if width:
+            self["width"] = width
+        if height:
+            self["height"] = height
+
+        text_rect = self.bbox(self.text)
+        if int(self["width"]) < text_rect[2]-text_rect[0]:
+            self["width"] = (text_rect[2]-text_rect[0]) + 10
+
+        if int(self["height"]) < text_rect[3]-text_rect[1]:
+            self["height"] = (text_rect[3]-text_rect[1]) + 10
+
+    def resize(self, event):
+        text_bbox = self.bbox(self.text)
+
+        if event.width < text_bbox[2]-text_bbox[0]:
+            width = text_bbox[2]-text_bbox[0] + 30
+        else:
+            width = event.width
+
+        if event.height < text_bbox[3]-text_bbox[1]:
+            height = text_bbox[3]-text_bbox[1] + 30
+        else:
+            height = event.height
+
+        self.coords(self.circle, 5, 5, width-5, height-5)
+
+        bbox = self.bbox(self.circle)
+
+        x = ((bbox[2]-bbox[0])/2) - ((text_bbox[2]-text_bbox[0])/2)
+        y = ((bbox[3]-bbox[1])/2) - ((text_bbox[3]-text_bbox[1])/2)
+
+        self.moveto(self.text, x, y)
+
+    def border(self, event):
+        if event.type == "4":
+            self.itemconfig(self.circle, fill="#d2d6d3")
+            if self.clicked is not None:
+                self.clicked()
+        else:
+            self.itemconfig(self.circle, fill=self.btnbackground)
+
+
+def func():
+    print("Button pressed")
+
+
+btn = RoundedButton(BOX_2,text="FUCK U", radius=100, btnbackground="#0078ff", width=200, height=200, btnforeground="#ffffff", clicked=func)
+btn.pack(expand=True, fill="both")
 
 
 
