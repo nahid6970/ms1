e9376e46a2b5d73331768a27b715fe252e711e3c
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2024-03-27 00:13:39 +0600

    XX

--- transparent_frame.py
+++ transparent_frame.py
@@ -16,7 +16,7 @@ win.geometry("700x250")
 win.wm_attributes('-transparentcolor', '#ab23ff')
 
 # Load the image
-image_path = "C:/Users/nahid/OneDrive/backup/icon/Dolphin_icon-20x20.png"
+image_path = "C:/Users/nahid/OneDrive/backup/icon/Dolphin_icon-small.png"
 image = Image.open(image_path)
 photo = ImageTk.PhotoImage(image)
 
@@ -25,11 +25,15 @@ transparent_label = Label(win, bg='#ab23ff')
 transparent_label.place(relx=0, rely=0, relwidth=1, relheight=1)
 
 # Create a Label with the image
-label = Label(transparent_label, image=photo)
+label = Label(transparent_label, image=photo, bg='#ab23ff')
 label.image = photo  # keep a reference to avoid garbage collection
-label.pack(ipadx=50, ipady=50, padx=20)
+label.pack(ipadx=1, ipady=1, padx=1)
 
 # Make the label clickable
 label.bind("<Button-1>", lambda event: open_directory())
+
+# Create a label for text
+text_label = Label(transparent_label, text="D Drive", font=("Helvetica", 12), bg='#ab23ff', fg="#fff")
+text_label.pack(pady=(0, 10))  # Adjust the padding as needed
 
 win.mainloop()
