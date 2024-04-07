1ebd85aeb7077509d66fbe01a2bed0f21adb25e0
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2024-04-07 09:50:28 +0600

    XX

--- mypygui_import/edit_files.py
+++ mypygui_import/edit_files.py
@@ -86,12 +86,17 @@ BOX_1 = tk.Frame(MAIN_FRAME, bg="#282c34")
 BOX_1.pack(side="top", pady=(30,0), padx=(0,0))
 
 button_properties = [
-("mypygui"         ,BOX_1,"#346c9a","#ffffff",1,47,"flat",("calibri",12,"bold"),0,0,(0,0),(0,0),lambda: subprocess.Popen(["Start","powershell","-Command","& { code C:\\ms1\\mypygui.py }"]         ,shell=True)                                                                     ),
-("desktop_icon"    ,BOX_1,"#346c9a","#ffffff",1,47,"flat",("calibri",12,"bold"),0,0,(0,0),(0,5),lambda: subprocess.Popen(["Start","powershell","-Command","& { code C:\\ms1\\desktop_icon.py }"]    ,shell=True)                                                                     ),
-("ahkscripts.ahk"  ,BOX_1,"#599b59","#ffffff",1,47,"flat",("calibri",12,"bold"),0,0,(0,0),(0,5),lambda: subprocess.Popen(["Start","powershell","-Command","& { code C:\\ms1\\ahkscripts.ahk }"]     ,shell=True)                                                                     ),
-("backup.ps1"      ,BOX_1,"#499eda","#ffffff",1,47,"flat",("calibri",12,"bold"),0,0,(0,0),(0,0),lambda: subprocess.Popen(["Start","powershell","-Command","& { code C:\\ms1\\backup.ps1 }"]         ,shell=True)                                                                     ),
-("startup.ps1"     ,BOX_1,"#499eda","#ffffff",1,47,"flat",("calibri",12,"bold"),0,0,(0,0),(0,0),lambda: subprocess.Popen(["Start","powershell","-Command","& { code C:\\ms1\\startup.ps1 }"]        ,shell=True)                                                                     ),
-("update.ps1"      ,BOX_1,"#499eda","#ffffff",1,47,"flat",("calibri",12,"bold"),0,0,(0,0),(0,0),lambda: subprocess.Popen(["Start","powershell","-Command","& { code C:\\ms1\\update.ps1 }"]         ,shell=True)                                                                     ),
+("mypygui.py"            ,BOX_1,"#346c9a","#ffffff",1,47,"flat",("calibri",12,"bold"),0,0,(0,0),(0,0),lambda: subprocess.Popen(["Start","powershell","-Command","& { code C:\\ms1\\mypygui.py }"]                                         ,shell=True)                                                                     ),
+("desktop_icon.py"       ,BOX_1,"#346c9a","#ffffff",1,47,"flat",("calibri",12,"bold"),0,0,(0,0),(0,5),lambda: subprocess.Popen(["Start","powershell","-Command","& { code C:\\ms1\\desktop_icon.py }"]                                    ,shell=True)                                                                     ),
+("edit_files.py"         ,BOX_1,"#346c9a","#ffffff",1,47,"flat",("calibri",12,"bold"),0,0,(0,0),(0,5),lambda: subprocess.Popen(["Start","powershell","-Command","& { code c:\\ms1\\mypygui_import\\edit_files.py }"]                      ,shell=True)                                                                     ),
+
+("ahkscripts.ahk"        ,BOX_1,"#599b59","#ffffff",1,47,"flat",("calibri",12,"bold"),0,0,(0,0),(0,5),lambda: subprocess.Popen(["Start","powershell","-Command","& { code C:\\ms1\\ahkscripts.ahk }"]                                     ,shell=True)                                                                     ),
+
+("backup.ps1"            ,BOX_1,"#499eda","#ffffff",1,47,"flat",("calibri",12,"bold"),0,0,(0,0),(0,0),lambda: subprocess.Popen(["Start","powershell","-Command","& { code C:\\ms1\\backup.ps1 }"]                                         ,shell=True)                                                                     ),
+("startup.ps1"           ,BOX_1,"#499eda","#ffffff",1,47,"flat",("calibri",12,"bold"),0,0,(0,0),(0,0),lambda: subprocess.Popen(["Start","powershell","-Command","& { code C:\\ms1\\startup.ps1 }"]                                        ,shell=True)                                                                     ),
+("update.ps1"            ,BOX_1,"#499eda","#ffffff",1,47,"flat",("calibri",12,"bold"),0,0,(0,0),(0,0),lambda: subprocess.Popen(["Start","powershell","-Command","& { code C:\\ms1\\update.ps1 }"]                                         ,shell=True)                                                                     ),
+
+("glaze-wm.yaml"         ,BOX_1,"#9068b0","#ffffff",1,47,"flat",("calibri",12,"bold"),0,0,(0,0),(5,0),lambda: subprocess.Popen(["Start","powershell","-Command","& { code C:\\Users\\nahid\\.glaze-wm\\config.yaml }"]                    ,shell=True)                                                                     ),
 ]
 for button_props in button_properties:
     create_button(*button_props)
