fb1cf746a1cdac2bfe5b149900c8e24f4aeddc70
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2024-04-12 16:34:52 +0600

    XX

--- running_apps.py
+++ running_apps.py
@@ -28,9 +28,9 @@ BORDER_FRAME = create_custom_border(root)
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 
-x = screen_width - 250
+x = screen_width - 500
 y = screen_height - 78
-root.geometry(f"250x30+{x}+{y}") #! overall size of the window
+root.geometry(f"500x30+{x}+{y}") #! overall size of the window
 
 root.wm_attributes('-transparentcolor', '#ab23ff')
 # Create a transparent label to overlay the image
@@ -46,30 +46,44 @@ def is_process_running(process_name):
             return True
     return False
 
+#! examples
+""" 
+"xxx.exe": is_process_running("xxx.exe"),
+(xxx_label       ,"xxx.exe"  ,"xxx"  ),
+xxx_label    =tk.Label(box1,bg="#fcc62a",fg="#000000",font=("JETBRAINSMONO NF",10,"bold"))
+xxx_label.grid(row=0,column=0,padx=5,pady=(3,1)) 
+
+"""
+
+
+
 # Function to update label visibility
 def update_labels():
     global last_statuses
     while True:
         statuses = {
-            "notepad.exe": is_process_running("notepad.exe"),
-            "whkd.exe": is_process_running("whkd.exe"),
-            "chrome.exe": is_process_running("chrome.exe"),
-            "Code.exe": is_process_running("Code.exe"),
-            "python.exe": is_process_running("python.exe"),
+            "notepad.exe" :is_process_running    ("notepad.exe" ),
+            "whkd.exe"    :is_process_running    ("whkd.exe"    ),
+            "chrome.exe"  :is_process_running    ("chrome.exe"  ),
+            "Code.exe"    :is_process_running    ("Code.exe"    ),
+            "python.exe"  :is_process_running    ("python.exe"  ),
+            "glazewm.exe" :is_process_running    ("glazewm.exe" ),
+            "komorebi.exe":is_process_running    ("komorebi.exe"),
         }
         if statuses != last_statuses:
             root.after_idle(update_labels_gui, statuses)
             last_statuses = statuses
         time.sleep(1)  # Check every 1 second
 
-# Function to update GUI labels
 def update_labels_gui(statuses):
     labels = [
-              (notepad_label       ,"notepad.exe"  ,"Notepad"  ),
-              (whkd_label          ,"whkd.exe"     ,"whkd"     ),
-              (chrome_label        ,"chrome.exe"   ,"Chrome"   ),
-              (Code_label          ,"Code.exe"     ,"Code"     ),
-              (Python_label        ,"python.exe"   ,"P"   ),
+              (notepad_label        ,"notepad.exe"   ,"Notepad"   ),
+              (whkd_label           ,"whkd.exe"      ,"whkd"      ),
+              (chrome_label         ,"chrome.exe"    ,"Chrome"    ),
+              (Code_label           ,"Code.exe"      ,"Code"      ),
+              (Python_label         ,"python.exe"    ,"Py"         ),
+              (glazewm_label        ,"glazewm.exe"   ,"glazewm"   ),
+              (komorebi_label       ,"komorebi.exe"  ,"komorebi"  ),
               ]
 
     for label, process_name, text in labels:
@@ -82,23 +96,24 @@ def update_labels_gui(statuses):
 box1 = tk.Frame(root, bg="#ab23ff")
 box1.pack(side="right", anchor="ne", pady=(1,2),padx=(3,1))
 
-# Create labels for each process
-chrome_label    =tk.Label(box1,bg="#fcc62a",fg="#000000",font=("JETBRAINSMONO NF",10,"bold"))
-whkd_label      =tk.Label(box1,bg="#FFFFFF",fg="#000000",font=("JETBRAINSMONO NF",10,"bold"))
-notepad_label   =tk.Label(box1,bg="#5bc0de",fg="#000000",font=("JETBRAINSMONO NF",10,"bold"))
-Code_label      =tk.Label(box1,bg="#23a9f2",fg="#000000",font=("JETBRAINSMONO NF",10,"bold"))
-Python_label    =tk.Label(box1,bg="#3772a4",fg="#000000",font=("JETBRAINSMONO NF",10,"bold"))
+chrome_label      =tk.Label(box1,bg="#fcc62a",fg="#000000",font=("JETBRAINSMONO NF",10,"bold"))
+whkd_label        =tk.Label(box1,bg="#FFFFFF",fg="#000000",font=("JETBRAINSMONO NF",10,"bold"))
+notepad_label     =tk.Label(box1,bg="#5bc0de",fg="#000000",font=("JETBRAINSMONO NF",10,"bold"))
+Code_label        =tk.Label(box1,bg="#23a9f2",fg="#000000",font=("JETBRAINSMONO NF",10,"bold"))
+Python_label      =tk.Label(box1,bg="#3772a4",fg="#000000",font=("JETBRAINSMONO NF",10,"bold"))
+glazewm_label     =tk.Label(box1,bg="#41bdf8",fg="#000000",font=("JETBRAINSMONO NF",10,"bold"))
+komorebi_label    =tk.Label(box1,bg="#9068b0",fg="#000000",font=("JETBRAINSMONO NF",10,"bold"))
 
 last_statuses = {}
 
-# Organize labels horizontally using grid
-notepad_label.grid(row=0,column=0,padx=5,pady=(3,1))
-whkd_label.grid   (row=0,column=1,padx=5,pady=(3,1))
-chrome_label.grid (row=0,column=2,padx=5,pady=(3,1))
-Code_label.grid   (row=0,column=3,padx=5,pady=(3,1))
-Python_label.grid (row=0,column=4,padx=5,pady=(3,1))
+notepad_label.grid (row=0,column=0,padx=5,pady=(3,1))
+whkd_label.grid    (row=0,column=1,padx=5,pady=(3,1))
+glazewm_label.grid (row=0,column=2,padx=5,pady=(3,1))
+komorebi_label.grid(row=0,column=3,padx=5,pady=(3,1))
+chrome_label.grid  (row=0,column=4,padx=5,pady=(3,1))
+Code_label.grid    (row=0,column=5,padx=5,pady=(3,1))
+Python_label.grid  (row=0,column=6,padx=5,pady=(3,1))
 
-# Start the label visibility update loop in a separate thread
 thread = threading.Thread(target=update_labels)
 thread.daemon = True
 thread.start()
