f0323418bf76d6c971f466abd70b34db2a66d426
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2024-04-12 15:02:34 +0600

    XX

--- running.py
+++ running.py
@@ -2,6 +2,9 @@ import tkinter as tk
 import psutil
 import threading
 import time
+from tkinter import *
+from PIL import Image, ImageTk
+import os
 
 def create_custom_border(parent):
     BORDER_FRAME = tk.Frame(parent, bg="#1d2027", bd=0, highlightthickness=1, highlightbackground="red")
@@ -12,7 +15,14 @@ root = tk.Tk()
 root.title("Process Monitor")
 root.configure(bg="#282c34")
 root.overrideredirect(True)  # Remove default borders
-root.attributes('-topmost', True)  # Set always on top
+# root.attributes('-topmost', True)  # Set always on top
+def check_window_topmost():
+    if not root.attributes('-topmost'):
+        root.attributes('-topmost', True)
+    root.after(500, check_window_topmost)
+# Call the function to check window topmost status periodically
+check_window_topmost()
+
 
 BORDER_FRAME = create_custom_border(root)
 screen_width = root.winfo_screenwidth()
@@ -21,6 +31,12 @@ screen_height = root.winfo_screenheight()
 x = screen_width - 250
 y = screen_height - 78
 root.geometry(f"250x30+{x}+{y}") #! overall size of the window
+
+root.wm_attributes('-transparentcolor', '#ab23ff')
+# Create a transparent label to overlay the image
+transparent_label = Label(root, bg='#ab23ff')
+transparent_label.place(relx=0, rely=0, relwidth=1, relheight=1)
+
 
 
 # Function to check if a process is running
@@ -39,6 +55,7 @@ def update_labels():
             "whkd.exe": is_process_running("whkd.exe"),
             "chrome.exe": is_process_running("chrome.exe"),
             "Code.exe": is_process_running("Code.exe"),
+            "python.exe": is_process_running("python.exe"),
         }
         if statuses != last_statuses:
             root.after_idle(update_labels_gui, statuses)
@@ -48,10 +65,11 @@ def update_labels():
 # Function to update GUI labels
 def update_labels_gui(statuses):
     labels = [
-              (notepad_label     ,"notepad.exe","Notepad"),
-              (whkd_label        ,"whkd.exe"   ,"whkd"   ),
-              (chrome_label      ,"chrome.exe" ,"Chrome" ),
-              (Code_label        ,"Code.exe"   ,"Code"   ),
+              (notepad_label       ,"notepad.exe"  ,"Notepad"  ),
+              (whkd_label          ,"whkd.exe"     ,"whkd"     ),
+              (chrome_label        ,"chrome.exe"   ,"Chrome"   ),
+              (Code_label          ,"Code.exe"     ,"Code"     ),
+              (Python_label        ,"python.exe"   ,"P"   ),
               ]
 
     for label, process_name, text in labels:
@@ -61,19 +79,24 @@ def update_labels_gui(statuses):
         else:
             label.grid_remove()
 
+box1 = tk.Frame(root, bg="#ab23ff")
+box1.pack(side="right", anchor="ne", pady=(1,2),padx=(3,1))
+
 # Create labels for each process
-chrome_label  =tk.Label(root,bg="#23a9f2",fg="#000000",font=("JETBRAINSMONO NF",10,"bold"))
-whkd_label    =tk.Label(root,bg="#23a9f2",fg="#000000",font=("JETBRAINSMONO NF",10,"bold"))
-notepad_label =tk.Label(root,bg="#23a9f2",fg="#000000",font=("JETBRAINSMONO NF",10,"bold"))
-Code_label    =tk.Label(root,bg="#23a9f2",fg="#000000",font=("JETBRAINSMONO NF",10,"bold"))
+chrome_label    =tk.Label(box1,bg="#fcc62a",fg="#000000",font=("JETBRAINSMONO NF",10,"bold"))
+whkd_label      =tk.Label(box1,bg="#FFFFFF",fg="#000000",font=("JETBRAINSMONO NF",10,"bold"))
+notepad_label   =tk.Label(box1,bg="#5bc0de",fg="#000000",font=("JETBRAINSMONO NF",10,"bold"))
+Code_label      =tk.Label(box1,bg="#23a9f2",fg="#000000",font=("JETBRAINSMONO NF",10,"bold"))
+Python_label    =tk.Label(box1,bg="#3772a4",fg="#000000",font=("JETBRAINSMONO NF",10,"bold"))
 
 last_statuses = {}
 
 # Organize labels horizontally using grid
-notepad_label.grid(row=0, column=0, padx=5, pady=5)
-whkd_label.grid(row=0, column=1, padx=5, pady=5)
-chrome_label.grid(row=0, column=2, padx=5, pady=5)
-Code_label.grid(row=0, column=3, padx=5, pady=5)
+notepad_label.grid(row=0,column=0,padx=5,pady=(3,1))
+whkd_label.grid   (row=0,column=1,padx=5,pady=(3,1))
+chrome_label.grid (row=0,column=2,padx=5,pady=(3,1))
+Code_label.grid   (row=0,column=3,padx=5,pady=(3,1))
+Python_label.grid (row=0,column=4,padx=5,pady=(3,1))
 
 # Start the label visibility update loop in a separate thread
 thread = threading.Thread(target=update_labels)
