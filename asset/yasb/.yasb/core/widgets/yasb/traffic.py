a7ce630a8dcb7a2aeab9199a18a27d2428c5a84b
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2024-04-14 16:13:17 +0600

    XX

--- asset/yasb/.yasb/core/widgets/yasb/traffic.py
+++ asset/yasb/.yasb/core/widgets/yasb/traffic.py
@@ -1,5 +1,4 @@
 import psutil
-from humanize import naturalsize
 from core.widgets.base import BaseWidget
 from core.validation.widgets.yasb.traffic import VALIDATION_SCHEMA
 from PyQt6.QtWidgets import QLabel
@@ -28,12 +27,11 @@ class TrafficWidget(BaseWidget):
         self._label_content = label
         self._label_alt_content = label_alt
 
-        self._label = QLabel()
-        self._label_alt = QLabel()
-        self._label.setProperty("class", "label")
-        self._label_alt.setProperty("class", "label alt")
-        self.widget_layout.addWidget(self._label)
-        self.widget_layout.addWidget(self._label_alt)
+        self._upload_label = QLabel()
+        self._download_label = QLabel()
+
+        self.widget_layout.addWidget(self._upload_label)
+        self.widget_layout.addWidget(self._download_label)
 
         self.register_callback("toggle_label", self._toggle_label)
         self.register_callback("update_label", self._update_label)
@@ -43,72 +41,59 @@ class TrafficWidget(BaseWidget):
         self.callback_middle = callbacks["on_middle"]
         self.callback_timer = "update_label"
 
-        self._label.show()
-        self._label_alt.hide()
-
         self.start_timer()
 
     def _toggle_label(self):
         self._show_alt_label = not self._show_alt_label
-
-        if self._show_alt_label:
-            self._label.hide()
-            self._label_alt.show()
-        else:
-            self._label.show()
-            self._label_alt.hide()
-
         self._update_label()
 
     def _update_label(self):
         # Update the active label at each timer interval
-        active_label = self._label_alt if self._show_alt_label else self._label
         active_label_content = self._label_alt_content if self._show_alt_label else self._label_content
-        active_label_formatted = active_label_content
 
         try:
             upload_speed, download_speed = self._get_speed()
         except Exception:
             upload_speed, download_speed = "N/A", "N/A"
 
-        label_options = [
-            ("{upload_speed}", upload_speed),
-            ("{download_speed}", download_speed),
-        ]
+        self._upload_label.setText("\udb80\uddda " + upload_speed)
+        self._download_label.setText("\udb81\udd52 " + download_speed)
 
-        for option, value in label_options:
-            active_label_formatted = active_label_formatted.replace(option, str(value))
+        self._set_label_color(self._upload_label, upload_speed)
+        self._set_label_color(self._download_label, download_speed)
 
-        active_label.setText(active_label_formatted)
+    def _set_label_color(self, label, speed):
+        speed_float = float(speed.split()[0])
 
-        # Set background color based on speed
-        upload_speed_float = float(upload_speed.split()[0])  # Extract the speed value as float
-        download_speed_float = float(download_speed.split()[0])  # Extract the speed value as float
-
-        if upload_speed_float < 1 and download_speed_float < 1:
-            active_label.setStyleSheet("background-color: #0daf15")  # Green color for speeds below 1MB/s
+        if speed_float == 0:
+            bg_color = "#1d2027"
+            fg_color = "#FFFFFF"
+        elif 0 < speed_float < 0.5:
+            bg_color = "#67D567"
+            fg_color = "#000000"
+        elif 0.5 <= speed_float < 1:
+            bg_color = "#32AB32"
+            fg_color = "#000000"
         else:
-            active_label.setStyleSheet("background-color: #51a2ff")  # Blue color for speeds above or equal to 1MB/s
+            bg_color = "#4b95e9"
+            fg_color = "#000000"
 
+        label.setStyleSheet(f"background-color: {bg_color}; color: {fg_color};")
 
     def _get_speed(self) -> [str, str]:
         current_io = psutil.net_io_counters()
-        upload_diff = current_io.bytes_sent - self.bytes_sent
         download_diff = current_io.bytes_recv - self.bytes_recv
+        upload_diff = current_io.bytes_sent - self.bytes_sent
 
         # Convert bytes to MB
-        upload_speed = upload_diff / (1024 * 1024 * self.interval)
         download_speed = download_diff / (1024 * 1024 * self.interval)
+        upload_speed = upload_diff / (1024 * 1024 * self.interval)
 
         # Format speeds to two decimal places
-        upload_speed_str = "{:.2f}".format(upload_speed)
         download_speed_str = "{:.2f}".format(download_speed)
-
-        # Format speeds as strings with 'MB/s' unit
-        upload_speed_formatted = f"{upload_speed_str}" # MB/s
-        download_speed_formatted = f"{download_speed_str}" # MB/s
+        upload_speed_str = "{:.2f}".format(upload_speed)
 
         self.bytes_sent = current_io.bytes_sent
         self.bytes_recv = current_io.bytes_recv
 
-        return upload_speed_formatted, download_speed_formatted
+        return download_speed_str, upload_speed_str
