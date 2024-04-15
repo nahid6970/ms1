83efe47b0847c2fc14f524f70e7cec5d20271e9e
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2024-04-15 14:01:23 +0600

    XX

--- utility/position_x_y.py
+++ utility/position_x_y.py
@@ -4,18 +4,39 @@ import pyautogui
 def update_position():
     x, y = pyautogui.position()
     position_label.config(text=f"X: {x}, Y: {y}")
-    root.after(1000, update_position)  # Schedule the update every 1000 milliseconds (1 second)
+    ROOT.after(1000, update_position)  # Schedule the update every 1000 milliseconds (1 second)
 
 # Create the main window
-root = tk.Tk()
-root.title("Mouse Position Tracker")
+ROOT = tk.Tk()
+ROOT.title("Mouse Position Tracker")
 
-# Create a label to display the mouse position
-position_label = tk.Label(root, text="")
-position_label.pack()
 
-# Start updating the mouse position
+
+def close_window(event=None):
+    ROOT.destroy()
+ROOT.attributes('-topmost', True)
+ROOT.configure(bg="#282c34")
+ROOT.overrideredirect(True)
+screen_width = ROOT.winfo_screenwidth()
+screen_height = ROOT.winfo_screenheight()
+x = 242
+y = 7
+ROOT.geometry(f"+{x}+{y}")
+ROOT.configure(bg="#282c34")
+ROOT.overrideredirect(True)
+box1 = tk.Frame(ROOT, bg="#1d2027")
+box1.pack(side="right", anchor="center", pady=(0,0),padx=(0,0))
+
+
+
+
+position_label = tk.Label(box1, text="", bg="#8de2d3", fg="#000000", font=("JETBRAINSMONO NF", 10, "bold"))
+position_label.pack(side="left")
+LB_name = tk.Label(box1, bg="#ff0000", fg="#FFFFFF", font=("JetBrainsMono NF", 10, "bold"), text="X")
+LB_name.pack(side="left")
+LB_name.bind("<Button-1>", close_window)
+
+
+
 update_position()
-
-# Run the Tkinter event loop
-root.mainloop()
+ROOT.mainloop()
