13491f6bae280656a3fca6d4f01251912da0ea59
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2024-04-17 14:00:48 +0600

    XX

--- asset/yasb/.yasb/core/widgets/yasb/PowerToys_Multi.py
+++ asset/yasb/.yasb/core/widgets/yasb/PowerToys_Multi.py
@@ -23,7 +23,7 @@ class PowerToysWidget(BaseWidget):
         update_interval: int,
         callbacks: dict[str, str],
     ):
-        super().__init__(update_interval, class_name="combined-widget")
+        super().__init__(update_interval, class_name="powertoys_combined-widget")
         self.interval = update_interval // 1000
 
         self._show_alt_label = False
@@ -33,10 +33,12 @@ class PowerToysWidget(BaseWidget):
         self.PowerToys_Mouse_Pointer = QLabel()
         self.PowerToys_Text_Extract = QLabel()
         self.PowerToys_Screen_Ruler = QLabel()
+        self.PowerToys_Screen_Color = QLabel()
 
         self.widget_layout.addWidget(self.PowerToys_Mouse_Pointer)
         self.widget_layout.addWidget(self.PowerToys_Text_Extract)
         self.widget_layout.addWidget(self.PowerToys_Screen_Ruler)
+        self.widget_layout.addWidget(self.PowerToys_Screen_Color)
 
         self.register_callback("toggle_label", self._toggle_label)
         self.register_callback("update_label", self._update_label)
@@ -47,41 +49,46 @@ class PowerToysWidget(BaseWidget):
         self.callback_timer = "update_label"
 
         self.start_timer()
-
     def _toggle_label(self):
         self._show_alt_label = not self._show_alt_label
         self._update_label()
-
     def _update_label(self):
         # Update the active label at each timer interval
         active_label_content = self._label_alt_content if self._show_alt_label else self._label_content
 
         # Set text and colors for additional labels
         self.PowerToys_Mouse_Pointer.setText("\uf245")
-        self.PowerToys_Text_Extract.setText("\udb80\udd4d")
+        self.PowerToys_Text_Extract.setText("\udb80\ude84")
         self.PowerToys_Screen_Ruler.setText("\udb84\udf53")
+        self.PowerToys_Screen_Color.setText("\ue275")
 
         self._set_label_color(self.PowerToys_Mouse_Pointer, "button_1")
         self._set_label_color(self.PowerToys_Text_Extract, "button_2")
         self._set_label_color(self.PowerToys_Screen_Ruler, "button_3")
+        self._set_label_color(self.PowerToys_Screen_Color, "button_4")
 
         self.PowerToys_Mouse_Pointer.mousePressEvent = lambda event: self._on_button_1_click(event, self.PowerToys_Mouse_Pointer)
         self.PowerToys_Text_Extract.mousePressEvent = lambda event: self._on_button_2_click(event, self.PowerToys_Text_Extract)
         self.PowerToys_Screen_Ruler.mousePressEvent = lambda event: self._on_button_3_click(event, self.PowerToys_Screen_Ruler)
+        self.PowerToys_Screen_Color.mousePressEvent = lambda event: self._on_button_4_click(event, self.PowerToys_Screen_Color)
 
     def _set_label_color(self, label, content):
         if content == "button_1":
-            bg_color = "#bcd12f"
+            bg_color = "#e75c1c"
             fg_color = "#000000" 
-            stylesheet = f"background-color: {bg_color}; color: {fg_color}; border: 1px solid black; border-radius: 5px;"
+            stylesheet = f"background-color: {bg_color}; color: {fg_color}; border: 1px solid black; border-radius: 5px; margin: 4px 3px;"
         elif content == "button_2":
-            bg_color = "#4285F4"
-            fg_color = "#FFFFFF"
-            stylesheet = f"background-color: {bg_color}; color: {fg_color}; border: 1px solid black; border-radius: 5px;"
-        elif content == "button_3":
-            bg_color = "#79e173"
+            bg_color = "#e75c1c"
             fg_color = "#000000"
-            stylesheet = f"background-color: {bg_color}; color: {fg_color}; border: 1px solid black; border-radius: 5px;"
+            stylesheet = f"background-color: {bg_color}; color: {fg_color}; border: 1px solid black; border-radius: 5px; margin: 4px 3px;"
+        elif content == "button_3":
+            bg_color = "#e75c1c"
+            fg_color = "#000000"
+            stylesheet = f"background-color: {bg_color}; color: {fg_color}; border: 1px solid black; border-radius: 5px; margin: 4px 3px;"
+        elif content == "button_4":
+            bg_color = "#e75c1c"
+            fg_color = "#000000"
+            stylesheet = f"background-color: {bg_color}; color: {fg_color}; border: 1px solid black; border-radius: 5px; margin: 4px 3px;"
 
         label.setStyleSheet(stylesheet)
 
@@ -98,6 +105,12 @@ class PowerToysWidget(BaseWidget):
            subprocess.Popen(['cmd.exe', '/c',''])
 
     def _on_button_3_click(self, event, label):
+        if event.button() == Qt.MouseButton.LeftButton:
+           subprocess.Popen(['cmd.exe', '/c', 'python C:\\ms1\\HotKeys.py powertoys_ruler'])
+        elif event.button() == Qt.MouseButton.RightButton:
+           subprocess.Popen(['cmd.exe', '/c',''])
+
+    def _on_button_4_click(self, event, label):
         if event.button() == Qt.MouseButton.LeftButton:
            subprocess.Popen(['cmd.exe', '/c', 'python C:\\ms1\\HotKeys.py powertoys_color_picker'])
         elif event.button() == Qt.MouseButton.RightButton:
