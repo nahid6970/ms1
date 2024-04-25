ee091b109ffc5a0a0a5f45202521cdb960754420
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2024-04-25 12:14:51 +0600

    XX

--- mypygui_import/app_store.py
+++ mypygui_import/app_store.py
@@ -143,7 +143,7 @@ def winget_infooo():
 
 
 def wget_unin_fzf():
-    command = ' $host.UI.RawUI.WindowTitle = "wget❌" ; winget list "" | fzf --multi --preview-window=up:60% --preview \'winget show {1}\' | ForEach-Object { winget uninstall $_.split()[0] }'
+    command = ' $host.UI.RawUI.WindowTitle = "wget❌" ; winget list | fzf --multi  | ForEach-Object { winget uninstall $_.split()[0] }' #!--preview-window=up:60% --preview \'winget show {1}\'
     try:
         subprocess.Popen([ 'start' , 'pwsh', '-Command', command], shell=True)
     except subprocess.CalledProcessError as e:
@@ -239,8 +239,9 @@ winget_scoop_button_properties = [
 ("Install"   ,winget_install     ,"#1D2027","#FFFFFF",1,12,"flat",("JetBrainsMono NF",10),5,5,0,5,     4,1,1 ,""  ),
 ("Uninstall" ,winget_uninst      ,"#1D2027","#FFFFFF",1,12,"flat",("JetBrainsMono NF",10),5,5,0,5,     5,1,1 ,""  ),
 ("Install"   ,lambda: subprocess.Popen(["Start", "pwsh",  "-NoExit", "-Command", "& {$host.UI.RawUI.WindowTitle = 'Winget_fzf_install' ; Set-Location ; C:\\ms1\\utility\\winget_scoop\\winget_fzf_install.ps1 ; cd ~}"],shell=True)     ,"#1D2027","#00FF00",1,12,"flat",("JetBrainsMono NF",10),5,5,0,5,     6,1,1 ,""  ),
-("Install2"   ,lambda: subprocess.Popen(["Start", "pwsh",  "-NoExit", "-Command", "& {$host.UI.RawUI.WindowTitle = 'Winget_fzf_install' ; Set-Location ; C:\\ms1\\utility\\winget_scoop\\winget_fzf_install2.ps1 ; cd ~}"],shell=True)     ,"#1D2027","#00FF00",1,12,"flat",("JetBrainsMono NF",10),5,5,0,5,     7,1,1 ,""  ),
-("Uninstall" ,wget_unin_fzf      ,"#1D2027","#FF0000",1,12,"flat",("JetBrainsMono NF",10),5,5,0,5,     8,1,1 ,""  ),
+("Install2"   ,lambda: subprocess.Popen(["Start", "pwsh",  "-NoExit", "-Command", "& {$host.UI.RawUI.WindowTitle = 'Winget_fzf_install2' ; Set-Location ; C:\\ms1\\utility\\winget_scoop\\winget_fzf_install2.ps1 ; cd ~}"],shell=True)     ,"#1D2027","#00FF00",1,12,"flat",("JetBrainsMono NF",10),5,5,0,5,     7,1,1 ,""  ),
+("Uninstall"   ,lambda: subprocess.Popen(["Start", "pwsh",  "-NoExit", "-Command", "& {$host.UI.RawUI.WindowTitle = 'Winget_fzf_Uninstall' ; Set-Location ; C:\\ms1\\utility\\winget_scoop\\winget_fzf_uninstall.ps1 ; cd ~}"],shell=True)     ,"#1D2027","#FF0000",1,12,"flat",("JetBrainsMono NF",10),5,5,0,5,     8,1,1 ,""  ),
+# ("Uninstall" ,wget_unin_fzf      ,"#1D2027","#FF0000",1,12,"flat",("JetBrainsMono NF",10),5,5,0,5,     8,1,1 ,""  ),
 
 ("Scoop"     ,None               ,"#95cd95","#000000",1,12,"flat",("JetBrainsMono NF",12),5,5,0,5,     1,2,1 ,"ew"),
 ("Search"    ,scoop_search       ,"#1D2027","#FFFFFF",1,12,"flat",("JetBrainsMono NF",10),5,5,0,5,     2,2,1 ,""  ),
---
+++ utility/winget_scoop/winget_fzf_uninstall.ps1
@@ -0,0 +1,71 @@
+# Function to search for packages using winget and fzf
+function SearchPackages {
+    $packageListFile = ".\package-Uninstall-list.txt"
+
+    # Check if the package list file exists and if it's older than 1 hour
+    if (Test-Path $packageListFile) {
+        $fileLastWriteTime = (Get-Item $packageListFile).LastWriteTime
+        $currentTime = Get-Date
+        $timeDifference = $currentTime - $fileLastWriteTime
+        if ($timeDifference.TotalHours -gt .001) {
+            # If the file is older than 1 hour, update the package list
+            UpdatePackageList
+        }
+    } else {
+        # If the file doesn't exist, update the package list
+        UpdatePackageList
+    }
+
+    # Load the package list from the file
+    $packages = Get-Content $packageListFile
+
+    $selectedPackage = $packages | fzf #!--preview="winget show {1}" --preview-window="right:20%"
+
+    if ($selectedPackage) {
+        $packageName = ($selectedPackage -split '\s+')[0]
+        $packageId = ($selectedPackage -split '\s+')[1]
+        InstallPackage $packageName $packageId
+    } else {
+        Write-Host "No package selected."
+    }
+}
+
+function UpdatePackageList {
+    $packageListFile = ".\package-Uninstall-list.txt"
+    # Delete the package list file if it exists
+    if (Test-Path $packageListFile) {
+        Remove-Item $packageListFile -Force
+    }
+    # Query winget search and filter out non-ASCII characters
+    $packages = winget list | Where-Object { $_ -notmatch '[^\x00-\x7F]' }
+    # Remove the first five rows
+    #! $packages = $packages | Select-Object -Skip 7
+    # Replace spaces with hyphens within the first 43 characters
+    $packages = $packages -replace '^((?:.{1,45})\S*)', { $_.Groups[1].Value -replace '\s', '-' }
+    $packages | Out-File $packageListFile -Encoding utf8
+    Write-Host "Package list updated."
+}
+
+
+# Function to install a selected package
+function InstallPackage {
+    param(
+        [string]$packageName,
+        [string]$packageId
+    )
+
+    Write-Host "Package name: $packageName"
+    Write-Host "Package ID: $packageId"
+    
+    $confirm = Read-Host "Do you want to Uninstall this package? (Y/N)"
+    if (-not $confirm -or $confirm -eq "Y" -or $confirm -eq "y") {
+        $installCommand = "winget uninstall --id $packageId"
+        Invoke-Expression $installCommand
+        Write-Host "Package Uninstallation initiated."
+    } else {
+        Write-Host "Package Uninstallation aborted."
+    }
+}
+
+# Call the function to search for packages
+SearchPackages
