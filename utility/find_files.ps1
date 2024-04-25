ecc8b0db0d44dd4d11e6cc4364ce661810d86012
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2024-04-25 18:37:18 +0600

    XX

--- utility/find_files.ps1
+++ utility/find_files.ps1
@@ -13,16 +13,19 @@ function OpenFileInVSCode {
 }
 
 # Main function to search directories and files using fzf
+# Main function to search directories and files using fzf with file preview
+# Main function to search directories and files using fzf with file preview
 function SearchDirectoriesAndFiles {
-    $selectedDirectory = ($directories | fzf)
+    $selectedDirectory = ($directories | fzf -m)
     if ($selectedDirectory) {
         $files = Get-ChildItem -Path $selectedDirectory -File -Recurse | Where-Object { $_.FullName -notmatch ($ignoreList -join '|') }
-        $selectedFile = ($files.FullName | fzf)
+        $selectedFile = ($files.FullName | fzf -m --preview "highlight -O ansi -l {}" --preview-window=top:50% )
         if ($selectedFile) {
             OpenFileInVSCode $selectedFile
         }
     }
 }
+
 
 # Call the main function
 SearchDirectoriesAndFiles
