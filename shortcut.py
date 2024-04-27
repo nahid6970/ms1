d8e0618e34c29283ed18ec4cf0789f3897e42850
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2024-04-27 16:12:40 +0600

    XX

--- shortcut.py
+++ shortcut.py
@@ -4,7 +4,10 @@ import keyboard
 import pygetwindow as gw
 import time
 
-# Create the main window
+def switch_to_frame(frame_to_show, frame_to_hide):
+    frame_to_hide.pack_forget()
+    frame_to_show.pack()
+
 root = tk.Tk()
 root.title("Shortcut Buttons")
 root.attributes('-topmost', True)
@@ -16,63 +19,10 @@ x = 0
 y = screen_height//2-800//2
 root.geometry(f"30x800+{x}+{y}")
 
-# Create a frame for the buttons
 Main_Window = tk.Frame(root, bg="#1d2027")
 Main_Window.pack(side="top", anchor="center", pady=(0,0), padx=(0,0))
 
 
-# # def send_k(window_title, shortcut):
-# #     # Find the window with the given title
-# #     app_window = gw.getWindowsWithTitle(window_title)[0]
-# #     app_window.activate()  # Activate the window
-# #     # Simulate the given shortcut
-# #     keyboard.send(shortcut)
-
-# def send_k(window_titles, shortcut):
-#     for title in window_titles:
-#         try:
-#             app_window = gw.getWindowsWithTitle(title)[0]
-#             app_window.activate()  # Activate the window
-#             keyboard.send(shortcut)
-#             return  # Exit the loop if shortcut sent successfully
-#         except IndexError:
-#             pass  # If window title not found, try the next one
-
-
-# def send_m_k(window_title, shortcuts):
-#     app_window = gw.getWindowsWithTitle(window_title)[0]
-#     app_window.activate()  # Activate the window
-#     # Loop through the list of shortcuts and send them one by one
-#     for shortcut in shortcuts:
-#         keyboard.send(shortcut)
-#         # Insert a small delay between each shortcut to ensure correct sequence
-#         time.sleep(0.1)
-
-
-
-def switch_to_frame(frame_to_show, frame_to_hide):
-    frame_to_hide.pack_forget()
-    frame_to_show.pack()
-
-# def create_button(text, frame, bg_color, fg_color, height, width, relief, font, row_button, column_button, rowspan_button, columnspan_button, sticky, padx_button, pady_button, padx_pack, pady_pack, command, window_title=None):
-#     button = tk.Button(frame, text=text, bg=bg_color, fg=fg_color, height=height, width=width, relief=relief, font=font, padx=padx_button, pady=pady_button, command=command)
-#     button.grid(row=row_button, column=column_button, rowspan=rowspan_button, columnspan=columnspan_button, padx=padx_pack, pady=pady_pack, sticky=sticky)
-#     button.window_title = window_title  # Store the window title with the button
-#     return button
-
-
-
-# vscode_window = tk.Button( root, text="Folder", command=lambda: switch_to_frame(vscode_window, root))
-# vscode_window = tk.Frame(bg="#1D2027")
-# vscode_window.pack_propagate(True)
-
-# Excel = tk.Button( root, text="Folder", command=lambda: switch_to_frame(Excel, root))
-# Excel = tk.Frame(bg="#1D2027")
-# Excel.pack_propagate(True)
-
-# terminal = tk.Button( root, text="Folder", command=lambda: switch_to_frame(terminal, root))
-# terminal = tk.Frame(bg="#1D2027")
-# terminal.pack_propagate(True)
 
 # Button properties: (text, frame, bg_color, fg_color, height, width, relief, font, row_button, column_button, rowspan_button, columnspan_button, sticky, padx_button, pady_button, padx_pack, pady_pack, command, window_title)
 button_properties=[
