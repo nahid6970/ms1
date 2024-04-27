c4ba8308d0835d71a344b318161662bcb6e0bdd8
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2024-04-27 16:11:24 +0600

    XX

--- shortcut.py
+++ shortcut.py
@@ -8,11 +8,13 @@ import time
 root = tk.Tk()
 root.title("Shortcut Buttons")
 root.attributes('-topmost', True)
+root.overrideredirect(True)  # Remove default borders
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
-x = screen_width-500
+
+x = 0
 y = screen_height//2-800//2
-root.geometry(f"500x800+{x}+{y}")
+root.geometry(f"30x800+{x}+{y}")
 
 # Create a frame for the buttons
 Main_Window = tk.Frame(root, bg="#1d2027")
@@ -52,25 +54,25 @@ def switch_to_frame(frame_to_show, frame_to_hide):
     frame_to_hide.pack_forget()
     frame_to_show.pack()
 
-def create_button(text, frame, bg_color, fg_color, height, width, relief, font, row_button, column_button, rowspan_button, columnspan_button, sticky, padx_button, pady_button, padx_pack, pady_pack, command, window_title=None):
-    button = tk.Button(frame, text=text, bg=bg_color, fg=fg_color, height=height, width=width, relief=relief, font=font, padx=padx_button, pady=pady_button, command=command)
-    button.grid(row=row_button, column=column_button, rowspan=rowspan_button, columnspan=columnspan_button, padx=padx_pack, pady=pady_pack, sticky=sticky)
-    button.window_title = window_title  # Store the window title with the button
-    return button
+# def create_button(text, frame, bg_color, fg_color, height, width, relief, font, row_button, column_button, rowspan_button, columnspan_button, sticky, padx_button, pady_button, padx_pack, pady_pack, command, window_title=None):
+#     button = tk.Button(frame, text=text, bg=bg_color, fg=fg_color, height=height, width=width, relief=relief, font=font, padx=padx_button, pady=pady_button, command=command)
+#     button.grid(row=row_button, column=column_button, rowspan=rowspan_button, columnspan=columnspan_button, padx=padx_pack, pady=pady_pack, sticky=sticky)
+#     button.window_title = window_title  # Store the window title with the button
+#     return button
 
 
 
-vscode_window = tk.Button( root, text="Folder", command=lambda: switch_to_frame(vscode_window, root))
-vscode_window = tk.Frame(bg="#1D2027")
-vscode_window.pack_propagate(True)
+# vscode_window = tk.Button( root, text="Folder", command=lambda: switch_to_frame(vscode_window, root))
+# vscode_window = tk.Frame(bg="#1D2027")
+# vscode_window.pack_propagate(True)
 
-Excel = tk.Button( root, text="Folder", command=lambda: switch_to_frame(Excel, root))
-Excel = tk.Frame(bg="#1D2027")
-Excel.pack_propagate(True)
+# Excel = tk.Button( root, text="Folder", command=lambda: switch_to_frame(Excel, root))
+# Excel = tk.Frame(bg="#1D2027")
+# Excel.pack_propagate(True)
 
-terminal = tk.Button( root, text="Folder", command=lambda: switch_to_frame(terminal, root))
-terminal = tk.Frame(bg="#1D2027")
-terminal.pack_propagate(True)
+# terminal = tk.Button( root, text="Folder", command=lambda: switch_to_frame(terminal, root))
+# terminal = tk.Frame(bg="#1D2027")
+# terminal.pack_propagate(True)
 
 # Button properties: (text, frame, bg_color, fg_color, height, width, relief, font, row_button, column_button, rowspan_button, columnspan_button, sticky, padx_button, pady_button, padx_pack, pady_pack, command, window_title)
 button_properties=[
@@ -119,25 +121,61 @@ button_properties=[
 ]
 
 
-# Button properties: (text, frame, bg_color, fg_color, height, width, relief, font, row_button, column_button, rowspan_button, columnspan_button, sticky, padx_button, pady_button, padx_pack, pady_pack, command, window_title)
-button_properties=[
-    ("VSCode", Main_Window, "#21a3f1", "#1D2027", 1, 0, "flat", ("JetBrainsMonoNF", 11, "bold"), 0, 0, 1, 1, "ew", 0, 0, (1, 1), (0, 0), lambda: switch_to_frame(vscode_window, Main_Window)),
-    ("RemoveDupLines", vscode_window, "#FFFFFF", "#1D2027", 1, 0, "flat", ("JetBrainsMonoNF", 11, "bold"), 16, 0, 1, 1, "ew", 0, 0, (1, 1), (0, 0),lambda:subprocess.Popen(["powershell", "python c:/ms1/HotKeys.py Series"])),
-]
+# # Button properties: (text, frame, bg_color, fg_color, height, width, relief, font, row_button, column_button, rowspan_button, columnspan_button, sticky, padx_button, pady_button, padx_pack, pady_pack, command, window_title)
+# button_properties=[
+#     ("VSCode", Main_Window, "#21a3f1", "#1D2027", 1, 0, "flat", ("JetBrainsMonoNF", 11, "bold"), 0, 0, 1, 1, "ew", 0, 0, (1, 1), (0, 0), lambda: switch_to_frame(vscode_window, Main_Window)),
+#     ("RemoveDupLines", vscode_window, "#FFFFFF", "#1D2027", 1, 0, "flat", ("JetBrainsMonoNF", 11, "bold"), 16, 0, 1, 1, "ew", 0, 0, (1, 1), (0, 0),lambda:subprocess.Popen(["powershell", "python c:/ms1/HotKeys.py Series"])),
+# ]
 
 
-# Create buttons
-for button_props in button_properties:
-    create_button(*button_props)
+# # Create buttons
+# for button_props in button_properties:
+#     create_button(*button_props)
 
 
 #! chrome
 #! ctrl+shift+b
 
-import customtkinter
-button = customtkinter.CTkButton(root,corner_radius=10,height=300, text="\uf011", command=None, hover_color="#154223",font=("JetBrainsMonoNL NFP", 20, "bold"))
-button.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
 
+class HoverButton(tk.Button):
+    def __init__(self, master=None, **kw):
+        self.bg = kw.pop('bg', "#000000")
+        self.h_bg = kw.pop('h_bg', "red")
+        self.default_fg = kw.pop('default_fg', "#FFFFFF")
+        self.h_fg = kw.pop('h_fg', "#000000")
+        super().__init__(master, **kw)
+        self.bind("<Enter>", self.on_enter)
+        self.bind("<Leave>", self.on_leave)
+        self.configure(bg=self.bg, fg=self.default_fg)
+
+    def on_enter(self, event):
+        self.configure(bg=self.h_bg, fg=self.h_fg)
+
+    def on_leave(self, event):
+        self.configure(bg=self.bg, fg=self.default_fg)
+
+default_font = ("Jetbrainsmono nf", 14, "italic")
+root.option_add("*Font", default_font)
+
+vscode_window = tk.Button( root, text="Folder", command=lambda: switch_to_frame(vscode_window, root))
+vscode_window = tk.Frame(bg="#1D2027")
+vscode_window.pack_propagate(True)
+
+Excel = tk.Button( root, text="Folder", command=lambda: switch_to_frame(Excel, root))
+Excel = tk.Frame(bg="#1D2027")
+Excel.pack_propagate(True)
+
+terminal = tk.Button( root, text="Folder", command=lambda: switch_to_frame(terminal, root))
+terminal = tk.Frame(bg="#1D2027")
+terminal.pack_propagate(True)
+
+
+
+VSCode = HoverButton(Main_Window, bg="#000000", fg="#FFFFFF", height=1, width=10, bd=0, highlightthickness=0, command=lambda:switch_to_frame(vscode_window , Main_Window), text="\udb81\ude10")
+VSCode.pack(side="top", anchor="center", padx=(0,0), pady=(0,0))
+
+AlignMultiCoulmnsbySeparator = HoverButton(vscode_window, bg="#000000", fg="#FFFFFF", height=1, width=20, bd=0, highlightthickness=0, font=("calibri", 14, "bold"), command=None, text="AA")
+AlignMultiCoulmnsbySeparator.pack(side="top", anchor="center", padx=(0,0), pady=(0,0))
 
 
 # Start the main event loop
