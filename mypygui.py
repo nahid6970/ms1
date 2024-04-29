55c9bf2eeef2e9490d2e5581b45873c4947e25b2
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2024-04-29 20:28:05 +0600

    XX

--- mypygui.py
+++ mypygui.py
@@ -456,23 +456,15 @@ def update_uptime_label():
 
 
 ROOT1 = tk.Frame(ROOT, bg="#1d2027")
-ROOT1.pack(side="left", pady=(1,2),padx=(5,1),  anchor="w", fill="x")
+ROOT1.pack(side="left", pady=(2,2),padx=(5,1),  anchor="w", fill="x")
 
 ROOT2 = tk.Frame(ROOT, bg="#1d2027")
-ROOT2.pack(side="left", pady=(1,2),padx=(0,0), expand=True, anchor="center", fill="x")
+ROOT2.pack(side="left", pady=(2,2),padx=(0,0), expand=True, anchor="center", fill="x")
 
 ROOT3 = tk.Frame(ROOT, bg="#1d2027")
-ROOT3.pack(side="right", pady=(1,2),padx=(5,1), anchor="e", fill="x")
+ROOT3.pack(side="right", pady=(2,2),padx=(5,1), anchor="e", fill="x")
 
-uptime_label   =tk.Label(ROOT2,bg="#1d2027",fg="#FFFFFF",height=0,width=0,relief="flat",highlightthickness=0,highlightbackground="#ffffff",anchor="w",font=("JetBrainsMono NF",10,"bold"),text=""            ) ; uptime_label.pack  ( side="top", padx=(350,0), pady= (0,0)) ; uptime_label.bind("<Button-1>", None)
-cpu_core_frame =tk.Frame(ROOT3,bg="#1d2027", highlightthickness=1, highlightbackground="#717d99", relief="solid") ; cpu_core_frame.pack( side="left",padx=(3,0), pady= (0,0))
-LB_CPU         =tk.Label(ROOT3,bg="#000000",fg="#FFFFFF",height=0,width=5,relief="flat",highlightthickness=0,highlightbackground="#ffffff",anchor="w",font=("JetBrainsMono NFP",10,"bold"),text=""            ) ; LB_CPU.pack        ( side="left",padx=(3,0), pady= (0,0)) ; LB_CPU.bind("<Button-1>", None)
-LB_GPU         =tk.Label(ROOT3,bg="#000000",fg="#FFFFFF",height=0,width=5,relief="flat",highlightthickness=0,highlightbackground="#ffffff",anchor="w",font=("JetBrainsMono NFP",10,"bold"),text=""            ) ; LB_GPU.pack        ( side="left",padx=(3,0), pady= (0,0)) ; LB_GPU.bind("<Button-1>", None)
-LB_RAM         =tk.Label(ROOT3,bg="#000000",fg="#FFFFFF",height=0,width=5,relief="flat",highlightthickness=0,highlightbackground="#ffffff",anchor="w",font=("JetBrainsMono NFP",10,"bold"),text=""            ) ; LB_RAM.pack        ( side="left",padx=(3,0), pady= (0,0)) ; LB_RAM.bind("<Button-1>", None)
-LB_DUC         =tk.Label(ROOT3,bg="#000000",fg="#FFFFFF",height=0,width=5,relief="flat",highlightthickness=0,highlightbackground="#ffffff",anchor="w",font=("JetBrainsMono NFP",10,"bold"),text=""            ) ; LB_DUC.pack        ( side="left",padx=(3,0), pady= (0,0)) ; LB_DUC.bind("<Button-1>", None)
-LB_DUD         =tk.Label(ROOT3,bg="#000000",fg="#FFFFFF",height=0,width=5,relief="flat",highlightthickness=0,highlightbackground="#ffffff",anchor="w",font=("JetBrainsMono NFP",10,"bold"),text=""            ) ; LB_DUD.pack        ( side="left",padx=(3,0), pady= (0,0)) ; LB_DUD.bind("<Button-1>", None)
-LB_UPLOAD      =tk.Label(ROOT3,bg="#000000",fg="#FFFFFF",height=0,width=7,relief="flat",highlightthickness=0,highlightbackground="#ffffff",anchor="w",font=("JetBrainsMono NFP",10,"bold"),text=""            ) ; LB_UPLOAD.pack     ( side="left",padx=(3,0), pady= (0,0)) ; LB_UPLOAD.bind("<Button-1>", None)
-LB_DWLOAD      =tk.Label(ROOT3,bg="#000000",fg="#FFFFFF",height=0,width=7,relief="flat",highlightthickness=0,highlightbackground="#ffffff",anchor="w",font=("JetBrainsMono NFP",10,"bold"),text=""            ) ; LB_DWLOAD.pack     ( side="left",padx=(3,0), pady= (0,0)) ; LB_DWLOAD.bind("<Button-1>", None)
+
 bkup           =tk.Label(ROOT1,bg="#1d2027",fg="#009fff",height=0,width=0,relief="flat",highlightthickness=0,highlightbackground="#ffffff",anchor="w",font=("JetBrainsMono NFP",16,"bold"),text="\udb80\udea2") ; bkup.pack          ( side="left",padx=(3,0), pady= (0,0)) ; bkup.bind("<Button-1>", start_backup)
 STATUS_MS1     =tk.Label(ROOT1,bg="#1d2027",fg="#FFFFFF",height=0,width=0,relief="flat",highlightthickness=0,highlightbackground="#ffffff",anchor="w",font=("JetBrainsMono NFP",10,"bold"),text=""            ) ; STATUS_MS1.pack    ( side="left",padx=(3,0), pady= (0,0)) ; STATUS_MS1.bind("<Button-1>", lambda event:show_git_changes("C:\\ms1"))
 STATUS_MS2     =tk.Label(ROOT1,bg="#1d2027",fg="#FFFFFF",height=0,width=0,relief="flat",highlightthickness=0,highlightbackground="#ffffff",anchor="w",font=("JetBrainsMono NFP",10,"bold"),text=""            ) ; STATUS_MS2.pack    ( side="left",padx=(3,0), pady= (0,0)) ; STATUS_MS2.bind("<Button-1>", lambda event:show_git_changes("C:\\ms2"))
@@ -481,30 +473,21 @@ LB_1           =tk.Label(ROOT1,bg="#1d2027",fg="#FFFFFF",height=0,width=0,relief
 BT_TOPMOST     =tk.Label(ROOT1,bg="#1d2027",fg="#FFFFFF",height=0,width=0,relief="flat",highlightthickness=0,highlightbackground="#ffffff",anchor="w",font=("JetBrainsMono NFP",12,"bold"),text="\udb81\udc03") ; BT_TOPMOST.pack    ( side="left",padx=(3,0), pady= (0,0)) ; BT_TOPMOST.bind("<Button-1>", lambda event:toggle_checking())
 CLEAR          =tk.Label(ROOT1,bg="#1d2027",fg="#FFFFFF",height=0,width=0,relief="flat",highlightthickness=0,highlightbackground="#ffffff",anchor="w",font=("JetBrainsMono NFP",12,"bold"),text="\ueabf"      ) ; CLEAR.pack         ( side="left",padx=(3,0), pady= (0,0)) ; CLEAR.bind("<Button-1>", lambda event:clear_screen())
 LB_get         =tk.Label(ROOT1,bg="#1d2027",fg="#ff0000",height=0,width=0,relief="flat",highlightthickness=0,highlightbackground="#ffffff",anchor="w",font=("JetBrainsMono NFP",12,"bold"),text="G"           ) ; LB_get.pack        ( side="left",padx=(3,0), pady= (0,0)) ; LB_get.bind("<Button-1>", lambda event:get_active_window_info())
+
+uptime_label   =tk.Label(ROOT2,bg="#1d2027",fg="#FFFFFF",height=0,width=0,relief="flat",highlightthickness=0,highlightbackground="#ffffff",anchor="w",font=("JetBrainsMono NF",10,"bold"),text=""            ) ; uptime_label.pack  ( side="top", padx=(350,0), pady= (0,0)) ; uptime_label.bind("<Button-1>", None)
+
+cpu_core_frame =tk.Frame(ROOT3,bg="#1d2027", highlightthickness=1, highlightbackground="#717d99", relief="solid") ; cpu_core_frame.pack( side="left",padx=(3,0), pady= (0,0))
+LB_CPU         =tk.Label(ROOT3,bg="#000000",fg="#FFFFFF",height=0,width=5,relief="flat",highlightthickness=0,highlightbackground="#ffffff",anchor="w",font=("JetBrainsMono NFP",10,"bold"),text=""            ) ; LB_CPU.pack        ( side="left",padx=(3,0), pady= (0,0)) ; LB_CPU.bind("<Button-1>", None)
+LB_GPU         =tk.Label(ROOT3,bg="#000000",fg="#FFFFFF",height=0,width=5,relief="flat",highlightthickness=0,highlightbackground="#ffffff",anchor="w",font=("JetBrainsMono NFP",10,"bold"),text=""            ) ; LB_GPU.pack        ( side="left",padx=(3,0), pady= (0,0)) ; LB_GPU.bind("<Button-1>", None)
+LB_RAM         =tk.Label(ROOT3,bg="#000000",fg="#FFFFFF",height=0,width=5,relief="flat",highlightthickness=0,highlightbackground="#ffffff",anchor="w",font=("JetBrainsMono NFP",10,"bold"),text=""            ) ; LB_RAM.pack        ( side="left",padx=(3,0), pady= (0,0)) ; LB_RAM.bind("<Button-1>", None)
+LB_DUC         =tk.Label(ROOT3,bg="#000000",fg="#FFFFFF",height=0,width=5,relief="flat",highlightthickness=0,highlightbackground="#ffffff",anchor="w",font=("JetBrainsMono NFP",10,"bold"),text=""            ) ; LB_DUC.pack        ( side="left",padx=(3,0), pady= (0,0)) ; LB_DUC.bind("<Button-1>", None)
+LB_DUD         =tk.Label(ROOT3,bg="#000000",fg="#FFFFFF",height=0,width=5,relief="flat",highlightthickness=0,highlightbackground="#ffffff",anchor="w",font=("JetBrainsMono NFP",10,"bold"),text=""            ) ; LB_DUD.pack        ( side="left",padx=(3,0), pady= (0,0)) ; LB_DUD.bind("<Button-1>", None)
+LB_UPLOAD      =tk.Label(ROOT3,bg="#000000",fg="#FFFFFF",height=0,width=7,relief="flat",highlightthickness=0,highlightbackground="#ffffff",anchor="w",font=("JetBrainsMono NFP",10,"bold"),text=""            ) ; LB_UPLOAD.pack     ( side="left",padx=(3,0), pady= (0,0)) ; LB_UPLOAD.bind("<Button-1>", None)
+LB_DWLOAD      =tk.Label(ROOT3,bg="#000000",fg="#FFFFFF",height=0,width=7,relief="flat",highlightthickness=0,highlightbackground="#ffffff",anchor="w",font=("JetBrainsMono NFP",10,"bold"),text=""            ) ; LB_DWLOAD.pack     ( side="left",padx=(3,0), pady= (0,0)) ; LB_DWLOAD.bind("<Button-1>", None)
 LB_R           =tk.Label(ROOT3,bg="#1d2027",fg="#26b2f3",height=0,width=0,relief="flat",highlightthickness=0,highlightbackground="#ffffff",anchor="w",font=("JetBrainsMono NFP",12,"bold"),text="\uf0e2"      ) ; LB_R.pack          ( side="left",padx=(3,0), pady= (0,0)) ; LB_R.bind("<Button-1>", restart)
 LB_L           =tk.Label(ROOT3,bg="#1d2027",fg="#00FF00",height=0,width=0,relief="flat",highlightthickness=0,highlightbackground="#ffffff",anchor="w",font=("JetBrainsMono NFP",16,"bold"),text="\uf106"      ) ; LB_L.pack          ( side="left",padx=(3,0), pady= (0,0)) ; LB_L.bind("<Button-1>", lambda event:toggle_window_size('line'))
 LB_M           =tk.Label(ROOT3,bg="#1d2027",fg="#26b2f3",height=0,width=0,relief="flat",highlightthickness=0,highlightbackground="#ffffff",anchor="w",font=("JetBrainsMono NFP",18,"bold"),text="\uea72"      ) ; LB_M.pack          ( side="left",padx=(3,0), pady= (0,0)) ; LB_M.bind("<Button-1>", lambda event:toggle_window_size('max'))
 LB_XXX         =tk.Label(ROOT3,bg="#1d2027",fg="#ff0000",height=0,width=0,relief="flat",highlightthickness=0,highlightbackground="#ffffff",anchor="w",font=("JetBrainsMono NFP",16,"bold"),text="\uf00d"      ) ; LB_XXX.pack        ( side="left",padx=(3,10), pady=(0,0)) ; LB_XXX.bind("<Button-1>", close_window)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
