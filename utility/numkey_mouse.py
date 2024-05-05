7609b98850ecbdc2ceca53c3f6ad208c4d9dd6d9
Nahid Ahmed <nahid6970@gmail.com>
2024-05-05 21:44:53 +0600

    XX

--- utility/numkey_mouse.py
+++ utility/numkey_mouse.py
@@ -6,23 +6,30 @@ pyautogui.FAILSAFE = False
 MOUSE_SPEED = 10
 
 def move_mouse_up_down():
-    while keyboard.is_pressed('up'):
+    while keyboard.is_pressed('w'):
         pyautogui.move(0, -MOUSE_SPEED)
-    while keyboard.is_pressed('down'):
+    while keyboard.is_pressed('s'):
         pyautogui.move(0, MOUSE_SPEED)
 
 def move_mouse_left_right():
-    while keyboard.is_pressed('left'):
+    while keyboard.is_pressed('a'):
         pyautogui.move(-MOUSE_SPEED, 0)
-    while keyboard.is_pressed('right'):
+    while keyboard.is_pressed('d'):
         pyautogui.move(MOUSE_SPEED, 0)
 
 def main():
-    print("Use arrow keys to move the mouse.")
+    print("Use WASD keys to move the mouse. Press Esc to exit.")
+    # Block the default functionality of WASD keys
+    keyboard.block_key('w')
+    keyboard.block_key('a')
+    keyboard.block_key('s')
+    keyboard.block_key('d')
     try:
         while True:
             move_mouse_up_down()
             move_mouse_left_right()
+            if keyboard.is_pressed('esc'):
+                break
     except KeyboardInterrupt:
         print("\nExiting...")
 
