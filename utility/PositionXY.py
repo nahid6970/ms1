3d84f2c72996332aa6dbf324bf28c70d4306a29c
Nahid Ahmed <nahid6970@gmail.com>
2024-06-10 20:53:38 +0600

    XX

--- utility/PositionXY.py
+++ utility/PositionXY.py
@@ -41,7 +41,7 @@ ROOT.bind("<Escape>", close_window)
 box1 = tk.Frame(ROOT, bg="#1d2027")
 box1.pack(side="right", anchor="center", pady=(0, 0), padx=(0, 0))
 
-position_label = tk.Label(box1, text="", bg="#8de2d3", fg="#000000", font=("JETBRAINSMONO NF", 10, "bold"))
+position_label = tk.Label(box1, text="", bg="#ffffff", fg="#000000", font=("JETBRAINSMONO NF", 10, "bold"))
 position_label.pack(side="left")
 
 # LB_name = tk.Label(box1, bg="#ff0000", fg="#FFFFFF", font=("JetBrainsMono NF", 10, "bold"), text="X")
