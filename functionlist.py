8683d32d115db74bcd200ebbad27985e8a928ca4
Nahid Ahmed <nahid6970@gmail.com>
2024-06-11 22:50:26 +0600

    XX

--- functionlist.py
+++ functionlist.py
@@ -228,135 +228,104 @@ whkd_dst         ="C:/ms1/asset/whkd/whkdrc/whkdrc"
 whkd_src         ="C:/Users/nahid/.config/whkdrc"
 
 #! App Path
-Zoxide_scoop_path = "C:\\Users\\nahid\\scoop\\apps\\zoxide\\current\\zoxide.exe"
-
-ripgrep_winget_path = "C:\\Users\\nahid\\AppData\\Local\\Microsoft\\WinGet\\Packages\\BurntSushi.ripgrep.MSVC_Microsoft.Winget.Source_8wekyb3d8bbwe\\ripgrep-14.1.0-x86_64-pc-windows-msvc\\rg.exe"
-ripgrep_scoop_path = "C:\\Users\\nahid\\scoop\\apps\\ripgrep\\current\\rg.exe"
-
-WinToy_winget_path = "C:\\Program Files\\WindowsApps\\11413PtruceanBogdan.Wintoys_1.3.0.0_x64__ankwhmsh70gj6\\Wintoys.exe"
-
-VSCodium_winget_path = "C:\\Users\\nahid\\AppData\\Local\\Programs\\VSCodium\\VSCodium.exe"
-
-whkd_scoop_path = "C:\\Users\\nahid\\scoop\\apps\\whkd\\current\\whkd.exe"
-
-komorebi_scoop_path = "C:\\Users\\nahid\\scoop\\apps\\komorebi\\current\\komorebi.exe"
-
-GlazeWM_scoop_path = "C:\\Users\\nahid\\scoop\\apps\\glazewm\\current\\GlazeWM.exe"
-
-X_mousebutton_winget_path = "C:\\Program Files\\Highresolution Enterprises\\X-Mouse Button Control\\XMouseButtonControl.exe"
-
-BulkUninstall_winget_path = "C:\\Program Files\\BCUninstaller\\BCUninstaller.exe"
-
-Ack_scoop_path = "C:\\Users\\nahid\\scoop\\apps\\ack\\current\\ack.bat"
-
-Adb_scoop_path = "C:\\Users\\nahid\\scoop\\apps\\adb\\current\\platform-tools\\adb.exe"
-
-Alacritty_scoop_path = "C:\\Users\\nahid\\scoop\\apps\\alacritty\\current\\alacritty.exe"
-Alacritty_winget_path = "C:\\Program Files\\Alacritty\\alacritty.exe"
-
-Aria2_scoop_path = "C:\\Users\\nahid\\scoop\\apps\\aria2\\current\\aria2c.exe"
-Aria2_winget_path = "C:\\Users\\nahid\\AppData\\Local\\Microsoft\\WinGet\\Links\\aria2c.exe"
-
-AudioRelay_winget_path = "C:\\Program Files (x86)\\AudioRelay\\AudioRelay.exe"
-
-AutoHotkeyv1_scoop_path = "C:\\Users\\nahid\\scoop\\apps\\autohotkey1.1\\current\\AutoHotkeyU64.exe"
-AutoHotkeyv1_winget_path = "C:\\Users\\nahid\\AppData\\Local\\Microsoft\\WindowsApps\\AutoHotkeyU64.exe"
-
-AutoHotkeyv2_winget_path = "C:\\Users\\nahid\\AppData\\Local\\Programs\\AutoHotkey\\v2\\AutoHotkey64.exe"
-
-Autoruns_winget_path="C:\\Users\\nahid\\AppData\\Local\\Microsoft\\WinGet\\Packages\\Microsoft.Sysinternals.Autoruns_Microsoft.Winget.Source_8wekyb3d8bbwe\\autoruns.exe"
-
-BareGrep_scoop_path="C:\\Users\\nahid\\scoop\\apps\\baregrep\\current\\baregrep.exe"
-
-Bat_scoop_path = "C:\\Users\\nahid\\scoop\\apps\\bat\\current\\bat.exe"
-
-Bazarr_winget_path = "C:\\Bazarr\\bazarr.py"
-
-Bitwarden_winget_path="C:\\Users\\nahid\\AppData\\Local\\Programs\\Bitwarden\\Bitwarden.exe"
-
-btop_scoop_path="C:\\Users\\nahid\\scoop\\apps\\btop\\current\\btop.exe"
-
-Capture2Text_scoop_path="C:\\Users\\nahid\\scoop\\apps\\capture2text\\current\\Capture2Text.exe"
-
-CheatEngine_scoop_path ="C:\\Users\\nahid\\scoop\\apps\\cheat-engine\\current\\Cheat Engine.exe"
-
-clink_scoop_path="C:\\Users\\nahid\\scoop\\apps\\clink\\current\\clink_x64.exe"
-
-Cmder_scoop_path="C:\\Users\\nahid\\scoop\\apps\\cmder\\current\\Cmder.exe"
-
-CPUZ_scoop_path="C:\\Users\\nahid\\scoop\\apps\\cpu-z\\current\\cpuz_x64.exe"
-
-CrystalDiskInfo_scoop_path="C:\\Users\\nahid\\scoop\\apps\\crystaldiskinfo\\current\\DiskInfo64.exe"
-
+Ack_scoop_path             ="C:\\Users\\nahid\\scoop\\apps\\ack\\current\\ack.bat"
+Adb_scoop_path             ="C:\\Users\\nahid\\scoop\\apps\\adb\\current\\platform-tools\\adb.exe"
+Alacritty_scoop_path       ="C:\\Users\\nahid\\scoop\\apps\\alacritty\\current\\alacritty.exe"
+Alacritty_winget_path      ="C:\\Program Files\\Alacritty\\alacritty.exe"
+Aria2_scoop_path           ="C:\\Users\\nahid\\scoop\\apps\\aria2\\current\\aria2c.exe"
+Aria2_winget_path          ="C:\\Users\\nahid\\AppData\\Local\\Microsoft\\WinGet\\Links\\aria2c.exe"
+AudioRelay_winget_path     ="C:\\Program Files (x86)\\AudioRelay\\AudioRelay.exe"
+AutoHotkeyv1_scoop_path    ="C:\\Users\\nahid\\scoop\\apps\\autohotkey1.1\\current\\AutoHotkeyU64.exe"
+AutoHotkeyv1_winget_path   ="C:\\Users\\nahid\\AppData\\Local\\Microsoft\\WindowsApps\\AutoHotkeyU64.exe"
+AutoHotkeyv2_winget_path   ="C:\\Users\\nahid\\AppData\\Local\\Programs\\AutoHotkey\\v2\\AutoHotkey64.exe"
+Autoruns_winget_path       ="C:\\Users\\nahid\\AppData\\Local\\Microsoft\\WinGet\\Packages\\Microsoft.Sysinternals.Autoruns_Microsoft.Winget.Source_8wekyb3d8bbwe\\autoruns.exe"
+BareGrep_scoop_path        ="C:\\Users\\nahid\\scoop\\apps\\baregrep\\current\\baregrep.exe"
+Bat_scoop_path             ="C:\\Users\\nahid\\scoop\\apps\\bat\\current\\bat.exe"
+Bazarr_winget_path         ="C:\\Bazarr\\bazarr.py"
+Bitwarden_winget_path      ="C:\\Users\\nahid\\AppData\\Local\\Programs\\Bitwarden\\Bitwarden.exe"
+btop_scoop_path            ="C:\\Users\\nahid\\scoop\\apps\\btop\\current\\btop.exe"
+BulkUninstall_winget_path  ="C:\\Program Files\\BCUninstaller\\BCUninstaller.exe"
+Capture2Text_scoop_path    ="C:\\Users\\nahid\\scoop\\apps\\capture2text\\current\\Capture2Text.exe"
+CheatEngine_scoop_path     ="C:\\Users\\nahid\\scoop\\apps\\cheat-engine\\current\\Cheat Engine.exe"
+clink_scoop_path           ="C:\\Users\\nahid\\scoop\\apps\\clink\\current\\clink_x64.exe"
+Cmder_scoop_path           ="C:\\Users\\nahid\\scoop\\apps\\cmder\\current\\Cmder.exe"
+CPUZ_scoop_path            ="C:\\Users\\nahid\\scoop\\apps\\cpu-z\\current\\cpuz_x64.exe"
+CrystalDiskInfo_scoop_path ="C:\\Users\\nahid\\scoop\\apps\\crystaldiskinfo\\current\\DiskInfo64.exe"
+Ditto_scoop_path           =r'C:\Users\nahid\scoop\apps\ditto\current\Ditto.exe'
+dotnet6_wp                 =r'C:\Program Files\dotnet\shared\Microsoft.NETCore.App\6.0.29\createdump.exe'
+DotNet8_wp                 =r"C:\Program Files\dotnet\dotnet.exe"
+eza_sp                     =r'C:\Users\nahid\scoop\apps\eza\current\eza.exe'
+fastfetch_sp               =r'C:\Users\nahid\scoop\apps\fastfetch\2.12.0\fastfetch.exe'
+ffmpeg_sp                  =r'C:\Users\nahid\scoop\apps\ffmpeg\current\bin\ffmpeg.exe'
+FFmpegBatch_sp             =r'C:\Users\nahid\scoop\apps\ffmpeg-batch\current\FFBatch.exe'
+FileConverter_wp           =r"C:\Program Files\File Converter\FileConverter.exe"
+filezillaServer_sp         =r'C:\Users\nahid\scoop\apps\filezilla-server\current\filezilla-server.exe'
+flaresolverr_sp            =r'C:\Users\nahid\scoop\apps\flaresolverr\current\flaresolverr.exe'
+FreeDownloadManager_wp     =r"C:\Users\nahid\AppData\Local\Softdeluxe\Free Download Manager\fdm.exe"
+fzf_sp                     =r'C:\Users\nahid\scoop\apps\fzf\current\fzf.exe'
+git_sp                     =r'C:\Users\nahid\scoop\apps\git\current\cmd\git.exe'
+GitHubDesktop_sp           =r'C:\Users\nahid\scoop\apps\github\current\GitHubDesktop.exe'
+GlazeWM_scoop_path         ="C:\\Users\\nahid\\scoop\\apps\\glazewm\\current\\GlazeWM.exe"
+grep_sp                    =r'C:\Users\nahid\scoop\apps\grep\current\grep.exe'
+highlight_sp               =r'C:\Users\nahid\scoop\apps\highlight\current\highlight.exe'
+imagemagick_sp             =r'C:\Users\nahid\scoop\apps\imagemagick\current\magick.exe'
+Inkscape_wp                =r"C:\Program Files\Inkscape\bin\inkscape.exe"
+Jackett_wp                 =r"C:\ProgramData\Jackett\JackettTray.exe"
+JavaRuntimeEnvironment_wp  =r"C:\Program Files (x86)\Common Files\Oracle\Java\javapath\java.exe"
+komorebi_scoop_path        ="C:\\Users\\nahid\\scoop\\apps\\komorebi\\current\\komorebi.exe"
+lazygit_sp                 =r'C:\Users\nahid\scoop\apps\lazygit\current\lazygit.exe'
+less_sp                    =r'C:\Users\nahid\scoop\apps\less\current\less.exe'
+localsend_sp               =r'C:\Users\nahid\scoop\apps\localsend\current\localsend_app.exe'
+meld_wp                    =r'C:\Program Files\Meld\Meld.exe'
+node_sp                    =r'C:\Users\nahid\scoop\apps\nodejs\current\node.exe'
+notepadplusplus_sp         =r'C:\Users\nahid\scoop\apps\notepadplusplus\current\notepad++.exe'
 notepadplusplus_winget_path="C:\\Program Files\\Notepad++\\notepad++.exe"
-notepadplusplus_sp=r'C:\Users\nahid\scoop\apps\notepadplusplus\current\notepad++.exe'
-
-Ditto_scoop_path = r'C:\Users\nahid\scoop\apps\ditto\current\Ditto.exe'
-DotNet8_wp = r"C:\Program Files\dotnet\dotnet.exe"
-eza_sp=r'C:\Users\nahid\scoop\apps\eza\current\eza.exe'
-FFmpegBatch_sp=r'C:\Users\nahid\scoop\apps\ffmpeg-batch\current\FFBatch.exe'
-ffmpeg_sp=r'C:\Users\nahid\scoop\apps\ffmpeg\current\bin\ffmpeg.exe'
-FileConverter_wp=r"C:\Program Files\File Converter\FileConverter.exe"
-filezillaServer_sp=r'C:\Users\nahid\scoop\apps\filezilla-server\current\filezilla-server.exe'
-flaresolverr_sp=r'C:\Users\nahid\scoop\apps\flaresolverr\current\flaresolverr.exe'
-FreeDownloadManager_wp=r"C:\Users\nahid\AppData\Local\Softdeluxe\Free Download Manager\fdm.exe"
-fzf_sp=r'C:\Users\nahid\scoop\apps\fzf\current\fzf.exe'
-git_sp=r'C:\Users\nahid\scoop\apps\git\current\cmd\git.exe'
-GitHubDesktop_sp=r'C:\Users\nahid\scoop\apps\github\current\GitHubDesktop.exe'
-grep_sp=r'C:\Users\nahid\scoop\apps\grep\current\grep.exe'
-highlight_sp=r'C:\Users\nahid\scoop\apps\highlight\current\highlight.exe'
-imagemagick_sp=r'C:\Users\nahid\scoop\apps\imagemagick\current\magick.exe'
-Inkscape_wp=r"C:\Program Files\Inkscape\bin\inkscape.exe"
-Jackett_wp=r"C:\ProgramData\Jackett\JackettTray.exe"
-JavaRuntimeEnvironment_wp=r"C:\Program Files (x86)\Common Files\Oracle\Java\javapath\java.exe"
-lazygit_sp=r'C:\Users\nahid\scoop\apps\lazygit\current\lazygit.exe'
-less_sp=r'C:\Users\nahid\scoop\apps\less\current\less.exe'
-localsend_sp=r'C:\Users\nahid\scoop\apps\localsend\current\localsend_app.exe'
-node_sp=r'C:\Users\nahid\scoop\apps\nodejs\current\node.exe'
-obsstudio_wp=r"C:\Program Files\obs-studio\bin\64bit\obs64.exe"
-ohmyposh_sp=r'C:\Users\nahid\scoop\apps\oh-my-posh\current\oh-my-posh.exe'
-pandoc_sp=r'C:\Users\nahid\scoop\apps\pandoc\current\pandoc.exe'
-perl_sp=r'C:\Users\nahid\scoop\apps\perl\current\perl\bin\perl.exe'
-php_sp=r'C:\Users\nahid\scoop\apps\php\current\php.exe'
-PotPlayer_wp=r"C:\Program Files\DAUM\PotPlayer\PotPlayerMini64.exe"
-PowerShell_wp=r"C:\Program Files\PowerShell\7\pwsh.exe"
-PowerToys_sp=r'C:\Users\nahid\scoop\apps\PowerToys\current\PowerToys.exe'
-PowerToys_wp=r"C:\Users\nahid\AppData\Local\PowerToys\PowerToys.exe"
-ProcessExplorer_wp=r"C:\Users\nahid\AppData\Local\Microsoft\WinGet\Packages\Microsoft.Sysinternals.ProcessExplorer_Microsoft.Winget.Source_8wekyb3d8bbwe\process-explorer.exe"
-Prowlarr_wp=r"C:\ProgramData\Prowlarr\bin\Prowlarr.exe"
-python_sp=r'C:\Users\nahid\scoop\apps\python\current\python.exe'
-qBittorrent_wp=r"C:\Program Files\qBittorrent\qbittorrent.exe"
-Radarr_wp=r"C:\ProgramData\Radarr\bin\Radarr.exe"
-Rclone_sp=r'C:\Users\nahid\scoop\apps\rclone\current\rclone.exe'
-Rclone_wp=r'C:\Users\nahid\AppData\Local\Microsoft\WinGet\Links\rclone.exe'
-ReIcon_sp=r'C:\Users\nahid\scoop\apps\reicon\current\ReIcon.exe'
-rssguard_sp=r'C:\Users\nahid\scoop\apps\rssguard\current\rssguard.exe'
-Ruffle_sp=r'C:\Users\nahid\scoop\apps\ruffle-nightly\current\ruffle.exe'
-Rufus_sp=r'C:\Users\nahid\scoop\apps\rufus\current\rufus.exe'
-Rufus_wp=r"C:\Users\nahid\AppData\Local\Microsoft\WinGet\Links\rufus.exe"
-
-scoopCompletion_sp=r'C:\Users\nahid\scoop\apps\scoop-completion\current\scoop-completion.psm1'
-scoopSearch_sp=r'C:\Users\nahid\scoop\apps\scoop-search\current\scoop-search.exe'
-
-scrcpy_sp=r'C:\Users\nahid\scoop\apps\scrcpy\current\scrcpy.exe'
-scrcpyplus_wp=r"C:\Users\nahid\AppData\Local\Programs\scrcpy-plus\scrcpy+.exe"
-Sonarr_wp=r"C:\ProgramData\Sonarr\bin\Sonarr.exe"
-steam_wp=r"C:\Program Files (x86)\Steam\steam.exe"
-Syncthing_sp=r'C:\Users\nahid\scoop\apps\syncthing\current\syncthing.exe'
-tldr_sp=r'C:\Users\nahid\scoop\apps\tldr\current\tldr.exe'
-VCredistAIO_sp=r'C:\Users\nahid\scoop\apps\vcredist-aio\current\VisualCppRedist_AIO_x86_x64.exe'
-VirtualBox_sp=r'C:\Users\nahid\scoop\apps\virtualbox-with-extension-pack-np\current\VirtualBox.exe'
-vscode_wp=r"C:\Users\nahid\AppData\Local\Programs\Microsoft VS Code\Code.exe"
-whatsapp_wp=r"C:\Program Files\WindowsApps\5319275A.WhatsAppDesktop_2.2407.10.0_x64__cv1g1gvanyjgm\WhatsApp.exe"
-WinaeroTweaker_sp=r'C:\Users\nahid\scoop\apps\winaero-tweaker\current\WinaeroTweaker.exe'
-windirstat_sp=r'C:\Users\nahid\scoop\apps\windirstat\current\windirstat.exe'
-winget_sp=r'C:\Users\nahid\scoop\apps\winget\current\winget.exe'
-WPUninstall_wp=r"C:\Program Files (x86)\Wise\Wise Program Uninstaller\WiseProgramUninstaller.exe"
-WizTree_sp=r'C:\Users\nahid\scoop\apps\wiztree\current\WizTree64.exe'
-wsapacman_sp=r'C:\Users\nahid\scoop\apps\wsa-pacman\current\WSA-pacman.exe'
-ytdlp_sp=r'C:\Users\nahid\scoop\apps\yt-dlp\current\yt-dlp.exe'
-WSUtilities_sp=r'C:\\Users\\nahid\\scoop\\apps\\workspaceutilities\\current\\WorkspaceUtilities.exe'
-meld_wp=r'C:\Program Files\Meld\Meld.exe'
-dotnet6_wp=r'C:\Program Files\dotnet\shared\Microsoft.NETCore.App\6.0.29\createdump.exe'
-fastfetch_sp=r'C:\Users\nahid\scoop\apps\fastfetch\2.12.0\fastfetch.exe'
-ventoy_sp=r'C:\Users\nahid\scoop\apps\ventoy\current\altexe\Ventoy2Disk_X64.exe'
+obsstudio_wp               =r"C:\Program Files\obs-studio\bin\64bit\obs64.exe"
+ohmyposh_sp                =r'C:\Users\nahid\scoop\apps\oh-my-posh\current\oh-my-posh.exe'
+pandoc_sp                  =r'C:\Users\nahid\scoop\apps\pandoc\current\pandoc.exe'
+perl_sp                    =r'C:\Users\nahid\scoop\apps\perl\current\perl\bin\perl.exe'
+php_sp                     =r'C:\Users\nahid\scoop\apps\php\current\php.exe'
+PotPlayer_wp               =r"C:\Program Files\DAUM\PotPlayer\PotPlayerMini64.exe"
+PowerShell_wp              =r"C:\Program Files\PowerShell\7\pwsh.exe"
+PowerToys_sp               =r'C:\Users\nahid\scoop\apps\PowerToys\current\PowerToys.exe'
+PowerToys_wp               =r"C:\Users\nahid\AppData\Local\PowerToys\PowerToys.exe"
+ProcessExplorer_wp         =r"C:\Users\nahid\AppData\Local\Microsoft\WinGet\Packages\Microsoft.Sysinternals.ProcessExplorer_Microsoft.Winget.Source_8wekyb3d8bbwe\process-explorer.exe"
+Prowlarr_wp                =r"C:\ProgramData\Prowlarr\bin\Prowlarr.exe"
+python_sp                  =r'C:\Users\nahid\scoop\apps\python\current\python.exe'
+qBittorrent_wp             =r"C:\Program Files\qBittorrent\qbittorrent.exe"
+Radarr_wp                  =r"C:\ProgramData\Radarr\bin\Radarr.exe"
+Rclone_sp                  =r'C:\Users\nahid\scoop\apps\rclone\current\rclone.exe'
+Rclone_wp                  =r'C:\Users\nahid\AppData\Local\Microsoft\WinGet\Links\rclone.exe'
+ReIcon_sp                  =r'C:\Users\nahid\scoop\apps\reicon\current\ReIcon.exe'
+ripgrep_scoop_path         ="C:\\Users\\nahid\\scoop\\apps\\ripgrep\\current\\rg.exe"
+ripgrep_winget_path        ="C:\\Users\\nahid\\AppData\\Local\\Microsoft\\WinGet\\Packages\\BurntSushi.ripgrep.MSVC_Microsoft.Winget.Source_8wekyb3d8bbwe\\ripgrep-14.1.0-x86_64-pc-windows-msvc\\rg.exe"
+rssguard_sp                =r'C:\Users\nahid\scoop\apps\rssguard\current\rssguard.exe'
+Ruffle_sp                  =r'C:\Users\nahid\scoop\apps\ruffle-nightly\current\ruffle.exe'
+Rufus_sp                   =r'C:\Users\nahid\scoop\apps\rufus\current\rufus.exe'
+Rufus_wp                   =r"C:\Users\nahid\AppData\Local\Microsoft\WinGet\Links\rufus.exe"
+scoopCompletion_sp         =r'C:\Users\nahid\scoop\apps\scoop-completion\current\scoop-completion.psm1'
+scoopSearch_sp             =r'C:\Users\nahid\scoop\apps\scoop-search\current\scoop-search.exe'
+scrcpy_sp                  =r'C:\Users\nahid\scoop\apps\scrcpy\current\scrcpy.exe'
+scrcpyplus_wp              =r"C:\Users\nahid\AppData\Local\Programs\scrcpy-plus\scrcpy+.exe"
+Sonarr_wp                  =r"C:\ProgramData\Sonarr\bin\Sonarr.exe"
+steam_wp                   =r"C:\Program Files (x86)\Steam\steam.exe"
+Syncthing_sp               =r'C:\Users\nahid\scoop\apps\syncthing\current\syncthing.exe'
+tldr_sp                    =r'C:\Users\nahid\scoop\apps\tldr\current\tldr.exe'
+VCredistAIO_sp             =r'C:\Users\nahid\scoop\apps\vcredist-aio\current\VisualCppRedist_AIO_x86_x64.exe'
+ventoy_sp                  =r'C:\Users\nahid\scoop\apps\ventoy\current\altexe\Ventoy2Disk_X64.exe'
+VirtualBox_sp              =r'C:\Users\nahid\scoop\apps\virtualbox-with-extension-pack-np\current\VirtualBox.exe'
+vscode_wp                  =r"C:\Users\nahid\AppData\Local\Programs\Microsoft VS Code\Code.exe"
+VSCodium_winget_path       ="C:\\Users\\nahid\\AppData\\Local\\Programs\\VSCodium\\VSCodium.exe"
+whatsapp_wp                =r"C:\Program Files\WindowsApps\5319275A.WhatsAppDesktop_2.2407.10.0_x64__cv1g1gvanyjgm\WhatsApp.exe"
+whkd_scoop_path            ="C:\\Users\\nahid\\scoop\\apps\\whkd\\current\\whkd.exe"
+WinaeroTweaker_sp          =r'C:\Users\nahid\scoop\apps\winaero-tweaker\current\WinaeroTweaker.exe'
+windirstat_sp              =r'C:\Users\nahid\scoop\apps\windirstat\current\windirstat.exe'
+winget_sp                  =r'C:\Users\nahid\scoop\apps\winget\current\winget.exe'
+WinToy_winget_path         ="C:\\Program Files\\WindowsApps\\11413PtruceanBogdan.Wintoys_1.3.0.0_x64__ankwhmsh70gj6\\Wintoys.exe"
+WizTree_sp                 =r'C:\Users\nahid\scoop\apps\wiztree\current\WizTree64.exe'
+WPUninstall_wp             =r"C:\Program Files (x86)\Wise\Wise Program Uninstaller\WiseProgramUninstaller.exe"
+wsapacman_sp               =r'C:\Users\nahid\scoop\apps\wsa-pacman\current\WSA-pacman.exe'
+WSUtilities_sp             =r'C:\\Users\\nahid\\scoop\\apps\\workspaceutilities\\current\\WorkspaceUtilities.exe'
+X_mousebutton_winget_path  ="C:\\Program Files\\Highresolution Enterprises\\X-Mouse Button Control\\XMouseButtonControl.exe"
+ytdlp_sp                   =r'C:\Users\nahid\scoop\apps\yt-dlp\current\yt-dlp.exe'
+Zoxide_scoop_path          ="C:\\Users\\nahid\\scoop\\apps\\zoxide\\current\\zoxide.exe"
