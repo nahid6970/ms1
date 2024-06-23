b533d74613b04686458a637f32ec5cc75e7807e1
Nahid Ahmed <nahid6970@gmail.com>
2024-06-23 21:49:05 +0600

    XX

--- scheduled.ps1
+++ scheduled.ps1
@@ -4,28 +4,11 @@ Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\SonarrList.txt -Val
 Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\SonarrList.txt -Value "########## $dateTime ##########"
 Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\SonarrList.txt -Value "#########################################"
 rclone tree 'D:\Downloads\@Sonarr' >> C:\Users\nahid\OneDrive\backup\FileListing\SonarrList.txt
-Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\SonarrList.txt -Value ""
-Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\SonarrList.txt -Value ""
-Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\SonarrList.txt -Value ""
-Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\SonarrList.txt -Value ""
-Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\SonarrList.txt -Value ""
-Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\SonarrList.txt -Value ""
-Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\SonarrList.txt -Value ""
-Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\SonarrList.txt -Value ""
-Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\SonarrList.txt -Value ""
-Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\SonarrList.txt -Value ""
+Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\SonarrList.txt -Value ("`n" * 10)
+
 
 Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\RadarrList.txt -Value "#########################################"
 Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\RadarrList.txt -Value "########## $dateTime ##########"
 Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\RadarrList.txt -Value "#########################################"
 rclone tree 'D:\Downloads\@Radarr' >> C:\Users\nahid\OneDrive\backup\FileListing\RadarrList.txt
-Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\RadarrList.txt -Value ""
-Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\RadarrList.txt -Value ""
-Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\RadarrList.txt -Value ""
-Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\RadarrList.txt -Value ""
-Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\RadarrList.txt -Value ""
-Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\RadarrList.txt -Value ""
-Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\RadarrList.txt -Value ""
-Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\RadarrList.txt -Value ""
-Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\RadarrList.txt -Value ""
-Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\RadarrList.txt -Value ""
+Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\RadarrList.txt -Value ("`n" * 10)
