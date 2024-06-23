374fc9bc85c0b257481eddd2c1d84e5ace361082
Nahid Ahmed <nahid6970@gmail.com>
2024-06-23 21:44:02 +0600

    XX

--- scheduled.ps1
+++ scheduled.ps1
@@ -1,8 +1,31 @@
 $dateTime = Get-Date -Format "yyyy-MM-dd hh:mm:ss"
 
-Add-Content -Path C:\ms1\FileTree.txt -Value "#########################################"
-Add-Content -Path C:\ms1\FileTree.txt -Value "########## $dateTime ##########"
-Add-Content -Path C:\ms1\FileTree.txt -Value "#########################################"
+Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\SonarrList.txt -Value "#########################################"
+Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\SonarrList.txt -Value "########## $dateTime ##########"
+Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\SonarrList.txt -Value "#########################################"
+rclone tree 'D:\Downloads\@Sonarr' >> C:\Users\nahid\OneDrive\backup\FileListing\SonarrList.txt
+Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\SonarrList.txt -Value ""
+Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\SonarrList.txt -Value ""
+Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\SonarrList.txt -Value ""
+Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\SonarrList.txt -Value ""
+Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\SonarrList.txt -Value ""
+Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\SonarrList.txt -Value ""
+Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\SonarrList.txt -Value ""
+Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\SonarrList.txt -Value ""
+Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\SonarrList.txt -Value ""
+Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\SonarrList.txt -Value ""
 
-rclone tree 'D:\Downloads\@Sonarr' >> C:\ms1\FileTree.txt
-rclone tree 'D:\Downloads\@Radarr' >> C:\ms1\FileTree.txt
+Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\RadarrList.txt -Value "#########################################"
+Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\RadarrList.txt -Value "########## $dateTime ##########"
+Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\RadarrList.txt -Value "#########################################"
+rclone tree 'D:\Downloads\@Radarr' >> C:\Users\nahid\OneDrive\backup\FileListing\RadarrList.txt
+Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\RadarrList.txt -Value ""
+Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\RadarrList.txt -Value ""
+Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\RadarrList.txt -Value ""
+Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\RadarrList.txt -Value ""
+Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\RadarrList.txt -Value ""
+Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\RadarrList.txt -Value ""
+Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\RadarrList.txt -Value ""
+Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\RadarrList.txt -Value ""
+Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\RadarrList.txt -Value ""
+Add-Content -Path C:\Users\nahid\OneDrive\backup\FileListing\RadarrList.txt -Value ""
