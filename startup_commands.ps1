373cd54f14040550add8cb60a90090b7863229c2
Nahid Ahmed <nahid6970@gmail.com>
2024-06-29 12:49:48 +0600

    xx

--- startup_commands.ps1
+++ startup_commands.ps1
@@ -1,4 +1,3 @@
-
 Start-Process 'C:\ms1\ahkscripts.ahk'
 Start-Process  'C:\ms1\mypygui.py' -WindowStyle Hidden
 komorebic start
@@ -7,7 +6,7 @@ Start-Process 'C:\ms1\utility\NetworkCondition.ps1' -WindowStyle Hidden
 Start-Process 'C:\Users\nahid\OneDrive\backup\win11-toggle-rounded-corners.exe' -ArgumentList '--disable' -Verb RunAs -WindowStyle Hidden
 Start-Process C:\ms1\scheduled.ps1
 Start-Process -FilePath 'C:\Users\nahid\scoop\apps\ditto\current\Ditto.exe'
-Start-Process -FilePath 'C:\ProgramData\Sonarr\bin\Sonarr.exe'
-Start-Process -FilePath 'C:\ProgramData\Radarr\bin\Radarr.exe'
 Start-Process -FilePath 'C:\ProgramData\Prowlarr\bin\Prowlarr.exe'
+Start-Process -FilePath 'C:\ProgramData\Radarr\bin\Radarr.exe'
 Start-Process -FilePath 'C:\Users\nahid\scoop\apps\rssguard\current\rssguard.exe'
+Start-Process -FilePath 'C:\ProgramData\Sonarr\bin\Sonarr.exe'
