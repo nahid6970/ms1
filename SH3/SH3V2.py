c3bf815f4bfacb407945526a236dbd1a711bbc46
Nahid Ahmed <nahid6970@gmail.com>
2024-09-15 15:24:40 +0600

    xx

--- SH3/SH3V2.py
+++ SH3/SH3V2.py
@@ -348,16 +348,16 @@ def perform_action(window):
                     holding_keys = False
                     break
                 # Perform the key presses
-                key_down(window, 'x')
+                # key_down(window, 'x')
                 key_down(window, 'd')
                 press_key(window, 'j')
                 press_key(window, 'j')
                 key_up(window, 'd')
-                key_up(window, 'x')
+                # key_up(window, 'x')
                 time.sleep(0.1)  # Small delay between iterations
             # Release keys after the action is completed
             key_up(window, 'd')
-            key_up(window, 'x')
+            # key_up(window, 'x')
             pause_other_items = False
         else:
             time.sleep(0.05)  # Prevent high CPU usage when idle
