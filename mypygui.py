b0e85bb9316e78664b431710ba5000d169d60fd4
Nahid Ahmed <nahid6970@gmail.com>
2024-09-15 18:18:21 +0600

    killpro.ps1

--- mypygui.py
+++ mypygui.py
@@ -763,7 +763,7 @@ Upload_lb.bind("<Button-1>",None)
 
 LB_CPU=tk.Label(ROOT2,bg="#000000",fg="#FFFFFF",height=0,width =5,relief="flat",highlightthickness=1,highlightbackground="#1b8af1",anchor ="center",font=("JetBrainsMono NFP",10,"bold"),text="")
 LB_CPU.pack(side="left",padx=(3,0 ),pady=(0,0))
-LB_CPU.bind("<Button-1>",lambda event:subprocess.Popen(["cmd /c start C:\\ms1\\utility\\killpro.ps1"], shell=True))
+LB_CPU.bind("<Button-1>",lambda event:subprocess.Popen([r"C:\ms1\utility\killpro.ps1"], shell=True))
 LB_CPU.bind("<Control-Button-1>",lambda event:subprocess.Popen(["cmd /c code C:\\ms1\\utility\\killpro.ps1"], shell=True))
 
 LB_GPU=tk.Label(ROOT2,bg="#000000",fg="#FFFFFF",height=0,width =5,relief="flat",highlightthickness=1,highlightbackground="#00ff21",anchor ="center",font=("JetBrainsMono NFP",10,"bold"),text="")
