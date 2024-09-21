839a7fe6f67d5499ad6207d4ead28a2d56623dc7
Nahid Ahmed <nahid6970@gmail.com>
2024-09-21 10:35:59 +0600

    modifying sf3 for ssh send music

--- ssh_to_termux.ps1
+++ ssh_to_termux.ps1
@@ -2,10 +2,10 @@
 $sshCommand = 'ssh u0_a827@192.168.0.102 -p 8022'
 
 # Start the SSH command in PowerShell
-Start-Process powershell -ArgumentList "-NoExit", "-Command", $sshCommand
+Start-Process pwsh -ArgumentList "-NoExit", "-Command", $sshCommand
 
 # Wait for the connection to be established
-Start-Sleep -Seconds 3  # Adjust as needed
+Start-Sleep -Seconds 5  # Adjust as needed
 
 # Type the command after connecting
 # Note: This will type in the active PowerShell window
