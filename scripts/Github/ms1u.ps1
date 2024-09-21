e90bcc010852d7c88e4a7ad6d9ca041661573090
Nahid Ahmed <nahid6970@gmail.com>
2024-09-22 00:11:14 +0600

    git script for ms123 changes

--- scripts/Github/ms1u.ps1
+++ scripts/Github/ms1u.ps1
@@ -8,21 +8,15 @@
 
 # Set the location to the repository directory
 Set-Location C:\ms1
-
 # Check the status of the repository
 git status
-
 # Add all changes to the staging area
 git add .
-
 # Prompt for a commit message
 $commitMessage = Read-Host "Enter commit message"
-
 # Commit the changes with the provided message
 git commit -m $commitMessage
-
 # Push the changes to the remote repository
 git push
-
 # Optionally, set the location back to the original directory
 Set-Location
--- scripts/Github/ms2u.ps1
+++ scripts/Github/ms2u.ps1
@@ -1,6 +1,24 @@
+# Set-Location C:\ms2
+# git status
+# git add .
+# git commit -m "XX"
+# git push
+# Set-Location
+
+
+
+
+# Set the location to the repository directory
 Set-Location C:\ms2
+# Check the status of the repository
 git status
+# Add all changes to the staging area
 git add .
-git commit -m "XX"
+# Prompt for a commit message
+$commitMessage = Read-Host "Enter commit message"
+# Commit the changes with the provided message
+git commit -m $commitMessage
+# Push the changes to the remote repository
 git push
+# Optionally, set the location back to the original directory
 Set-Location
