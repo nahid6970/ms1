8ae6c0598aeeb1aebadb1185e6243ebce832fae3
Nahid Ahmed <nahid6970@gmail.com>
2024-09-30 19:24:03 +0600

    xx

--- mypygui_import/applist.py
+++ mypygui_import/applist.py
@@ -5,7 +5,7 @@ from tkinter import Canvas, Scrollbar
 from tkinter import ttk
 import os
 import ctypes
-from functionlist import *
+from Path_Var import *
 
 # import sys
 # sys.path.append('C:/ms1/')
