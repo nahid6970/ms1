a7dc762f70f7dc0841f354158f2cba723e72c6c4
Nahid Ahmed <nahid6970@gmail.com>
2024-10-17 14:06:24 +0600

    xx

--- mypygui.py
+++ mypygui.py
@@ -608,9 +608,7 @@ VirtualMonitor_lb.bind("<Control-Button-1>",lambda event=None:subprocess.Popen('
 
 ShadowFight3_lb = tk.Label(ROOT1,text="sf3", bg="#1d2027", fg="#cc5907", height=0, width=0, relief="flat", highlightthickness=0, highlightbackground="#ffffff", anchor="w", font=("JetBrainsMono NFP", 16, "bold"))
 ShadowFight3_lb.pack(side="left", padx=(0, 0), pady=(0, 0))
-ShadowFight3_lb.bind("<Button-1>", lambda event=None: subprocess.Popen(
-    ['python', 'C:\\ms1\\SH3\\SH3V2.py'], creationflags=subprocess.CREATE_NEW_CONSOLE
-))
+ShadowFight3_lb.bind("<Button-1>",lambda event=None:subprocess.Popen('cmd /c start C:\\ms1\\SH3\\SH3V2.py'))
 ShadowFight3_lb.bind("<Control-Button-1>",lambda event=None:subprocess.Popen('cmd /c code C:\\ms1\\SH3\\SH3V2.py'))
 ShadowFight3_lb.bind("<Button-3>",lambda event=None:subprocess.Popen('cmd /c start C:\\ms1\\SH3\\SH3V2__AHK.py'))
 ShadowFight3_lb.bind("<Control-Button-3>",lambda event=None:subprocess.Popen('cmd /c code C:\\ms1\\SH3\\SH3V2__AHK.py'))
