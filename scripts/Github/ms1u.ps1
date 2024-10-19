c45b56dc65156dbc6f2159d266842b503667d1f3
Nahid Ahmed <nahid6970@gmail.com>
2024-10-19 12:38:18 +0600

    [Files Changed] ⚡ ms1u.ps1

--- scripts/Github/ms1u.ps1
+++ scripts/Github/ms1u.ps1
@@ -247,10 +247,10 @@ if ($commitMessage -like "xx*") {
     # Construct the final commit message
     if ($extraComment -ne '') {
         # If there's an extra comment, add the 💬 emoji before it
-        $commitMessage = "󰅿 $extraComment --- $fileList"
+        $commitMessage = "󰅿 $extraComment [Files Changed] $fileList"
     } else {
         # If there's no extra comment, just include the file list
-        $commitMessage = "--- $fileList"
+        $commitMessage = "[Files Changed] $fileList"
     }
 }
 
