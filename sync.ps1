b3d54b8f53da8702fc1f9ffe399620dbd606d7bc
nahid <nahid6970@gmail.com>
2025-01-06 21:39:24 +0600

     sync.ps1

--- sync.ps1
+++ sync.ps1
@@ -15,21 +15,24 @@ $host.UI.RawUI.WindowTitle = "Sync"
 for ($i = 1; ; $i++) {
     New-Variable -Name "var$i" -Value $i
     Get-Variable -Name "var$i" -ValueOnly
-powershell Write-Host "STORAGE INFO" -ForegroundColor black -BackgroundColor green
-    rclone about gu:
-#backups of mx
-powershell Write-Host "MX" -ForegroundColor black -BackgroundColor white
-    rclone  sync $mx_src $mx_dst -P  --ignore-existing --transfers=1 --track-renames --exclude @ignore/ --track-renames-strategy=modtime --fast-list --max-size 1M --log-level ERROR  --log-level INFO --log-file="C:\Users\nahid\OneDrive\backup\rclone\mx.txt" #--order-by size,asc --stats-file-name-length "8"
-    rclone  sync $mx_src $mx_dst -P  --ignore-existing --transfers=1 --track-renames --exclude @ignore/ --track-renames-strategy=modtime --fast-list                                  --log-level INFO --log-file="C:\Users\nahid\OneDrive\backup\rclone\mx.txt" #--order-by size,asc --stats-file-name-length "8"
-#backups of song
-powershell Write-Host "song" -ForegroundColor black -BackgroundColor white
-    rclone sync $song_src $song_dst -P --check-first --transfers=1 --track-renames --fast-list --log-level INFO --log-file="C:\Users\nahid\OneDrive\backup\rclone\song.txt"
-#backups of software
-powershell Write-Host "software" -ForegroundColor black -BackgroundColor white
-    rclone sync $software_src $software_dst -P --check-first --transfers=5 --track-renames --exclude @ignore/ --fast-list --max-size 1M --log-level INFO --log-file="C:\Users\nahid\OneDrive\backup\rclone\software.txt"
-    rclone sync $software_src $software_dst -P --check-first --transfers=1 --track-renames --exclude @ignore/ --fast-list               --log-level INFO --log-file="C:\Users\nahid\OneDrive\backup\rclone\software.txt"
-    
-timeout /T 360 /NOBREAK
+# powershell Write-Host "STORAGE INFO" -ForegroundColor black -BackgroundColor green
+#     rclone about gu:
+# #backups of mx
+# powershell Write-Host "MX" -ForegroundColor black -BackgroundColor white
+#     rclone  sync $mx_src $mx_dst -P  --ignore-existing --transfers=1 --track-renames --exclude @ignore/ --track-renames-strategy=modtime --fast-list --max-size 1M --log-level ERROR  --log-level INFO --log-file="C:\Users\nahid\OneDrive\backup\rclone\mx.txt" #--order-by size,asc --stats-file-name-length "8"
+#     rclone  sync $mx_src $mx_dst -P  --ignore-existing --transfers=1 --track-renames --exclude @ignore/ --track-renames-strategy=modtime --fast-list                                  --log-level INFO --log-file="C:\Users\nahid\OneDrive\backup\rclone\mx.txt" #--order-by size,asc --stats-file-name-length "8"
+# #backups of song
+# powershell Write-Host "song" -ForegroundColor black -BackgroundColor white
+#     rclone sync $song_src $song_dst -P --check-first --transfers=1 --track-renames --fast-list --log-level INFO --log-file="C:\Users\nahid\OneDrive\backup\rclone\song.txt"
+# #backups of software
+# powershell Write-Host "software" -ForegroundColor black -BackgroundColor white
+#     rclone sync $software_src $software_dst -P --check-first --transfers=5 --track-renames --exclude @ignore/ --fast-list --max-size 1M --log-level INFO --log-file="C:\Users\nahid\OneDrive\backup\rclone\software.txt"
+#     rclone sync $software_src $software_dst -P --check-first --transfers=1 --track-renames --exclude @ignore/ --fast-list               --log-level INFO --log-file="C:\Users\nahid\OneDrive\backup\rclone\software.txt"
+
+ Copy-Item -Path C:\msBackups\sf3_img.txt -Destination C:\msBackups\sf3_img2.txt
+ rclone copy C:\msBackups\sf3_img2.txt g00:\msBackups\sf3_img2.txt -P --check-first --transfers=10 --track-renames --fast-list
+
+timeout /T 10 /NOBREAK
 Clear-Host
 }
 
