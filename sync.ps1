e280f636e6b6cb6a1992dbece5b675b45ff7b4fa
nahid <nahid6970@gmail.com>
2025-01-22 12:37:30 +0600

    January 22, 2025, 12:37 PM

--- sync.ps1
+++ sync.ps1
@@ -4,13 +4,16 @@ for ($i = 1; ; $i++) {
     Get-Variable -Name "var$i" -ValueOnly
 # powershell Write-Host "STORAGE INFO" -ForegroundColor black -BackgroundColor green
 #     rclone about gu:
+
 # #backups of mx
 # powershell Write-Host "MX" -ForegroundColor black -BackgroundColor white
 #     rclone  sync "D:\mi9t\mx" "cgu:" -P  --ignore-existing --transfers=1 --track-renames --exclude @ignore/ --track-renames-strategy=modtime --fast-list --max-size 1M --log-level ERROR  --log-level INFO --log-file="C:\Users\nahid\OneDrive\backup\rclone\mx.txt" #--order-by size,asc --stats-file-name-length "8"
 #     rclone  sync "D:\mi9t\mx" "cgu:" -P  --ignore-existing --transfers=1 --track-renames --exclude @ignore/ --track-renames-strategy=modtime --fast-list                                  --log-level INFO --log-file="C:\Users\nahid\OneDrive\backup\rclone\mx.txt" #--order-by size,asc --stats-file-name-length "8"
+
 # #backups of song
 # powershell Write-Host "song" -ForegroundColor black -BackgroundColor white
 #     rclone sync "D:/song" "gu:song" -P --check-first --transfers=1 --track-renames --fast-list --log-level INFO --log-file="C:\Users\nahid\OneDrive\backup\rclone\song.txt"
+
 # #backups of software
 # powershell Write-Host "software" -ForegroundColor black -BackgroundColor white
 #     rclone sync "D:/software" "gu:/software" -P --check-first --transfers=5 --track-renames --exclude @ignore/ --fast-list --max-size 1M --log-level INFO --log-file="C:\Users\nahid\OneDrive\backup\rclone\software.txt"
