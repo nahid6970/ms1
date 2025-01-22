753835c5a5703c836a0280ad0229a1de6a52dba6
nahid <nahid6970@gmail.com>
2025-01-22 12:36:10 +0600

    January 22, 2025, 12:36 PM

--- sync.ps1
+++ sync.ps1
@@ -1,14 +1,3 @@
-#Define Variables
-# $mx_src = "D:\mi9t\mx"
-# $mx_dst = "cgu:"
-
-# $song_src = "D:/song"
-# $song_dst = "gu:song"
-
-# $software_src = "D:/software"
-# $software_dst = "gu:/software"
-
-
 $host.UI.RawUI.WindowTitle = "Sync"
 for ($i = 1; ; $i++) {
     New-Variable -Name "var$i" -Value $i
@@ -17,15 +6,15 @@ for ($i = 1; ; $i++) {
 #     rclone about gu:
 # #backups of mx
 # powershell Write-Host "MX" -ForegroundColor black -BackgroundColor white
-#     rclone  sync $mx_src $mx_dst -P  --ignore-existing --transfers=1 --track-renames --exclude @ignore/ --track-renames-strategy=modtime --fast-list --max-size 1M --log-level ERROR  --log-level INFO --log-file="C:\Users\nahid\OneDrive\backup\rclone\mx.txt" #--order-by size,asc --stats-file-name-length "8"
-#     rclone  sync $mx_src $mx_dst -P  --ignore-existing --transfers=1 --track-renames --exclude @ignore/ --track-renames-strategy=modtime --fast-list                                  --log-level INFO --log-file="C:\Users\nahid\OneDrive\backup\rclone\mx.txt" #--order-by size,asc --stats-file-name-length "8"
+#     rclone  sync "D:\mi9t\mx" "cgu:" -P  --ignore-existing --transfers=1 --track-renames --exclude @ignore/ --track-renames-strategy=modtime --fast-list --max-size 1M --log-level ERROR  --log-level INFO --log-file="C:\Users\nahid\OneDrive\backup\rclone\mx.txt" #--order-by size,asc --stats-file-name-length "8"
+#     rclone  sync "D:\mi9t\mx" "cgu:" -P  --ignore-existing --transfers=1 --track-renames --exclude @ignore/ --track-renames-strategy=modtime --fast-list                                  --log-level INFO --log-file="C:\Users\nahid\OneDrive\backup\rclone\mx.txt" #--order-by size,asc --stats-file-name-length "8"
 # #backups of song
 # powershell Write-Host "song" -ForegroundColor black -BackgroundColor white
-#     rclone sync $song_src $song_dst -P --check-first --transfers=1 --track-renames --fast-list --log-level INFO --log-file="C:\Users\nahid\OneDrive\backup\rclone\song.txt"
+#     rclone sync "D:/song" "gu:song" -P --check-first --transfers=1 --track-renames --fast-list --log-level INFO --log-file="C:\Users\nahid\OneDrive\backup\rclone\song.txt"
 # #backups of software
 # powershell Write-Host "software" -ForegroundColor black -BackgroundColor white
-#     rclone sync $software_src $software_dst -P --check-first --transfers=5 --track-renames --exclude @ignore/ --fast-list --max-size 1M --log-level INFO --log-file="C:\Users\nahid\OneDrive\backup\rclone\software.txt"
-#     rclone sync $software_src $software_dst -P --check-first --transfers=1 --track-renames --exclude @ignore/ --fast-list               --log-level INFO --log-file="C:\Users\nahid\OneDrive\backup\rclone\software.txt"
+#     rclone sync "D:/software" "gu:/software" -P --check-first --transfers=5 --track-renames --exclude @ignore/ --fast-list --max-size 1M --log-level INFO --log-file="C:\Users\nahid\OneDrive\backup\rclone\software.txt"
+#     rclone sync "D:/software" "gu:/software" -P --check-first --transfers=1 --track-renames --exclude @ignore/ --fast-list               --log-level INFO --log-file="C:\Users\nahid\OneDrive\backup\rclone\software.txt"
 
 rclone sync C:\msBackups\ o0:\msBackups\ -P --check-first --transfers=10 --track-renames --fast-list
 
