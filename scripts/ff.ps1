38c72980c9cf12e9676167a18728856c41d2efe5
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2025-04-25 21:44:32 +0600

    📅 April 25, 2025, 09:44 PM 🚀 ff added run command

--- scripts/ff.ps1
+++ scripts/ff.ps1
@@ -13,20 +13,20 @@ $ignoreList = @(".git", ".pyc")
 
 # Main function
 function SearchDirectoriesAndFiles {
-  Get-ChildItem -Path $directories -File -Recurse -ErrorAction SilentlyContinue |
-      Where-Object { $_.FullName -notmatch ($ignoreList -join '|') } |
-      ForEach-Object { "$($_.FullName)`t$($_.DirectoryName)" } |
-      fzf `
-          --multi `
-          --with-nth=1 `
-          --delimiter="`t" `
-          --preview="highlight -O ansi -l {1}" `
-          --preview-window=top:30% `
-          --bind "enter:execute-silent(code {+1})" `
-          --bind "ctrl-o:execute-silent(explorer.exe {+2})" `
-          --bind "ctrl-c:execute-silent(cmd /c echo {+1} | clip)"
+    Get-ChildItem -Path $directories -File -Recurse -ErrorAction SilentlyContinue |
+        Where-Object { $_.FullName -notmatch ($ignoreList -join '|') } |
+        ForEach-Object { "$($_.FullName)`t$($_.DirectoryName)" } |
+        fzf `
+            --multi `
+            --with-nth=1 `
+            --delimiter="`t" `
+            --preview="highlight -O ansi -l {1}" `
+            --preview-window=top:30% `
+            --bind "enter:execute-silent(code {+1})" `
+            --bind "ctrl-o:execute-silent(explorer.exe {+2})" `
+            --bind "ctrl-c:execute-silent(cmd /c echo {+1} | clip)" `
+            --bind "ctrl-r:execute-silent(powershell -command Start-Process '{+1}')"
 }
-
 
 # Call main
 SearchDirectoriesAndFiles
