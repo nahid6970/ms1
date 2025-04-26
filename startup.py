2deb081fce35db62c7d3aefca870bd8ad60e95cf
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2025-04-26 13:49:22 +0600

    📅 April 26, 2025, 01:49 PM

--- startup.py
+++ startup.py
@@ -34,6 +34,7 @@ class StartupManager(tk.Tk):
 {"type": "Command","name": "Flask - 5006 - Run Commands"      ,"paths": [r"C:\Users\nahid\scoop\apps\python312\current\pythonw.exe"] , "Command": "C:\\ms1\\scripts\\flask\\5006_Run_Commands\\Run_Commands.py"},
 {"type": "Command","name": "Flask - 5007 - ControlBy Android" ,"paths": [r"C:\Users\nahid\scoop\apps\python312\current\pythonw.exe"] , "Command": "C:\\ms1\\scripts\\flask\\5007_Controlby_Android\\Android_Control_PC.py"},
 {"type": "Command","name": "Flask - 5008 - Valorant Match"    ,"paths": [r"C:\Users\nahid\scoop\apps\python312\current\pythonw.exe"] , "Command": "C:\\ms1\\scripts\\flask\\5008_esports_scheduler\\Esports_Match_Schedule.py"},
+{"type": "Command","name": "Flask - 5009 - MCQ"               ,"paths": [r"C:\Users\nahid\scoop\apps\python312\current\pythonw.exe"] , "Command": "C:\\ms1\\scripts\\flask\\5009_MCQ\\app.py"},
 {"type": "Command","name": "Komorebi"                         ,"paths": [r"C:\ms1\scripts\Autohtokey\Command\komorebi.ahk"]},
 {"type": "Command","name": "mypygui"                          ,"paths": [r"C:\ms1\scripts\Autohtokey\Command\mypygui.ahk"]},
 {"type": "Command","name": "Open WebUI"                       ,"paths": [r"C:\ms1\scripts\Autohtokey\Command\openwebui.ahk"]},
