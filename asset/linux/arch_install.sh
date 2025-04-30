a8ea735e499cfa4af93fa4cea58a8e8bfa3b5fd6
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2025-04-30 08:33:24 +0600

    📅 April 30, 2025, 08:33 AM 🚀 test

--- asset/linux/arch_install.sh
+++ asset/linux/arch_install.sh
@@ -31,20 +31,6 @@ setup_user_password() {
     echo -e "${GREEN}Username and password saved.${NC}"
 }
 
-# Function to select install disk
-select_install_disk() {
-    clear
-    lsblk
-    echo
-    read -p "Enter the disk to install (example: /dev/sda): " INSTALL_DISK
-    echo
-    read -p "⚠️  WARNING: This will erase all data on $INSTALL_DISK. Are you sure? (yes/no): " confirm
-    if [[ "$confirm" != "yes" ]]; then
-        echo "Installation cancelled."
-        exit 1
-    fi
-}
-
 # Function to install base system
 install_base_system() {
     clear
@@ -158,7 +144,6 @@ exit_script() {
 }
 
 setup_user_password
-select_install_disk
 install_base_system
 # install_aur_helper
 # install_kde
