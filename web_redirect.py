1a379b60c4ba15a32b6d4d5564ce99041c3be269
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2025-05-01 00:58:39 +0600

    📅 May 01, 2025, 12:58 AM 🚀 web redirect flask

---
+++ web_redirect.py
@@ -0,0 +1,17 @@
+from flask import Flask, redirect, send_file
+
+app = Flask(__name__)
+
+# Port 7710: Send local file
+@app.route('/')
+def serve_local_file():
+    return send_file(r'C:\ms1\asset\linux\arch_install.sh', as_attachment=True)
+
+# Run the apps on their respective ports
+if __name__ == '__main__':
+    from threading import Thread
+
+    def run_app(app, port):
+        app.run(host='0.0.0.0', port=port, debug=False)
+
+    Thread(target=run_app, args=(app, 7710)).start()
