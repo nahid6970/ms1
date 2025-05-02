02ca66417b967d1ffe88c49820baa7591e162f68
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2025-05-02 12:19:58 +0600

    📅 May 02, 2025, 12:19 PM

--- archlinux/os.sh
+++ archlinux/os.sh
@@ -18,35 +18,33 @@ BASHRC_DEST="$HOME/.bashrc"
 NVIM_INIT_SOURCE="$REPO_DIR/dotfiles/neovim/init.lua"
 NVIM_CONFIG_DEST="$HOME/.config/nvim"
 
-# Function to install necessary packages
+# List of packages to install via yay
 packages=(
-    "bash"
-    "bat"
-    "chafa"
-    "curl"
-    "eza"
-    "fastfetch"
-    "fzf"
-    "git"
-    "lsd"
-    "lua-language-server"
-    "nano"
-    "neovim"
-    "oh-my-posh"
-    "openssh"
-    "python"
-    "rclone"
-    "sshpass"
-    "wget"
-    "which" # to fix neovim bug lua language server not supported on this platform
-    "yazi"
-    "zoxide"
-    "zsh"
-    # "mpv"
-    # "vim"
-    # "x11-repo" "tigervnc"
-    # "xdotool"
+    "bash" "bat" "chafa" "curl" "eza" "fastfetch" "fzf" "git" "lsd" "lua-language-server" "nano"
+    "neovim" "oh-my-posh" "openssh" "python" "rclone" "sshpass" "wget" "which" "yazi" "zoxide" "zsh"
 )
+
+# Function to install necessary packages using yay
+install_packages() {
+    clear
+    echo -e "${GREEN}Updating package database...${NC}"
+    yay -Sy --noconfirm
+
+    echo -e "${GREEN}Installing necessary packages...${NC}"
+    for pkg in "${packages[@]}"; do
+        if ! pacman -Q $pkg &>/dev/null; then
+            echo -e "${GREEN}Installing $pkg...${NC}"
+            if yay -S --noconfirm "$pkg"; then
+                echo -e "${GREEN}$pkg installed successfully.${NC}"
+            else
+                echo -e "${RED}Failed to install $pkg.${NC}"
+            fi
+        else
+            echo -e "${YELLOW}$pkg is already installed.${NC}"
+        fi
+    done
+}
+
 
 # Function to install necessary packages
 install_packages() {
