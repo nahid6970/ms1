78c440270796f790be9827cf629485a52de54343
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2025-05-02 10:49:40 +0600

    📅 May 02, 2025, 10:49 AM

--- archlinux/os.sh
+++ archlinux/os.sh
@@ -84,7 +84,7 @@ install_jetbrains_mono_font() {
     # Install oh-my-posh if not already installed
     if ! command -v oh-my-posh &> /dev/null; then
         echo -e "\e[33moh-my-posh not found. Installing...\e[0m"
-        sudo yay -Sy --noconfirm oh-my-posh
+        yay -S oh-my-posh
     fi
 
     # Install the font using oh-my-posh
