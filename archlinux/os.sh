d10bbcb012b84418461e263bb682f378bdddf505
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2025-05-02 12:16:50 +0600

    📅 May 02, 2025, 12:16 PM

--- archlinux/os.sh
+++ archlinux/os.sh
@@ -14,9 +14,7 @@ NC='\033[0m' # No Color
 
 REPO_DIR="$HOME/ms1"
 BASHRC_SOURCE="$REPO_DIR/archlinux/config/bashrc"
-TERMUX_PROPERTIES_SOURCE="$REPO_DIR/termux.properties"
 BASHRC_DEST="$HOME/.bashrc"
-TERMUX_PROPERTIES_DEST="$HOME/.termux/termux.properties"
 NVIM_INIT_SOURCE="$REPO_DIR/dotfiles/neovim/init.lua"
 NVIM_CONFIG_DEST="$HOME/.config/nvim"
 
