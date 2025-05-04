a72ad02c93f56399e49e02307c954e40d06f0e51
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2025-05-04 12:46:12 +0600

    📅 May 04, 2025, 12:46 PM

--- archlinux/os.sh
+++ archlinux/os.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # Define some color variables
-GREEN='\e[0;32m'
-RED='\e[0;31m'
-YELLOW='\e[1;33m'
-NC='\e[0m' # No Color
-
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
 
 # Define some variables
 # storage="$HOME/storage/shared"
