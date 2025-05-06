f2e64566472df5ea201a6ae65c94d04c11b8e694
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2025-05-07 02:02:36 +0600

    📅 May 07, 2025, 02:02 AM

---
+++ archlinux/dwmblocks_dt_fixed/blocks.def.h
@@ -0,0 +1,12 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+    {" 🐧 ", "/home/nahid/opt/dwmblocks-distrotube/scripts/kernel",   360, 2},
+    {" 🔺 ", "/home/nahid/opt/dwmblocks-distrotube/scripts/upt",      60,  2},
+    {" 📦 ", "/home/nahid/opt/dwmblocks-distrotube/scripts/pacupdate",360, 9},
+    {" 💻 ", "/home/nahid/opt/dwmblocks-distrotube/scripts/memory",   6,   1},
+    {" 🔊 ", "/home/nahid/opt/dwmblocks-distrotube/scripts/volume",   0,   10},
+    {" 🕑 ", "/home/nahid/opt/dwmblocks-distrotube/scripts/clock",    60,  0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim = '|';
