24d7f68fd5395a0641720b6fa5352e70d9c7d777
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2025-05-07 02:03:56 +0600

    📅 May 07, 2025, 02:03 AM

--- archlinux/dwmblocks_dt_fixed/blocks.def.h
+++ archlinux/dwmblocks_dt_fixed/blocks.def.h
@@ -1,11 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-    {" 🐧 ", "/home/nahid/opt/dwmblocks-distrotube/scripts/kernel",   360, 2},
-    {" 🔺 ", "/home/nahid/opt/dwmblocks-distrotube/scripts/upt",      60,  2},
-    {" 📦 ", "/home/nahid/opt/dwmblocks-distrotube/scripts/pacupdate",360, 9},
-    {" 💻 ", "/home/nahid/opt/dwmblocks-distrotube/scripts/memory",   6,   1},
-    {" 🔊 ", "/home/nahid/opt/dwmblocks-distrotube/scripts/volume",   0,   10},
-    {" 🕑 ", "/home/nahid/opt/dwmblocks-distrotube/scripts/clock",    60,  0},
+    {" 🐧 ", "/home/nahid/ms1/archlinux/dwmblocks_dt_fixed/scripts/kernel",   360, 2},
+    {" 🔺 ", "/home/nahid/ms1/archlinux/dwmblocks_dt_fixed/scripts/upt",      60,  2},
+    {" 📦 ", "/home/nahid/ms1/archlinux/dwmblocks_dt_fixed/scripts/pacupdate",360, 9},
+    {" 💻 ", "/home/nahid/ms1/archlinux/dwmblocks_dt_fixed/scripts/memory",   6,   1},
+    {" 🔊 ", "/home/nahid/ms1/archlinux/dwmblocks_dt_fixed/scripts/volume",   0,   10},
+    {" 🕑 ", "/home/nahid/ms1/archlinux/dwmblocks_dt_fixed/scripts/clock",    60,  0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
