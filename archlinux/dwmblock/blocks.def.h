ad3dbc0a9a972dab3d5547ed7e9c67bdf715a786
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2025-05-07 02:12:40 +0600

    📅 May 07, 2025, 02:12 AM

--- archlinux/dwmblock/blocks.def.h
+++ archlinux/dwmblock/blocks.def.h
@@ -1,9 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+    {" 🐧 ", "/home/nahid/ms1/archlinux/dwmblock/scripts/kernel",   360, 2},
+    {" 🔺 ", "/home/nahid/ms1/archlinux/dwmblock/scripts/upt",      60,  2},
+    {" 📦 ", "/home/nahid/ms1/archlinux/dwmblock/scripts/pacupdate",360, 9},
+    {" 💻 ", "/home/nahid/ms1/archlinux/dwmblock/scripts/memory",   6,   1},
+    {" 🔊 ", "/home/nahid/ms1/archlinux/dwmblock/scripts/volume",   0,   10},
+    {" 🕑 ", "/home/nahid/ms1/archlinux/dwmblock/scripts/clock",    60,  0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
