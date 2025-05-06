57845dd11113bfa1aa6892904ed79bc093db859c
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2025-05-06 12:12:01 +0600

    📅 May 06, 2025, 12:12 PM

--- archlinux/dwm/autostart.sh
+++ archlinux/dwm/autostart.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-while true; do
-    xsetroot -name "$(date)"
-    sleep 1s
-done &
-
 dte() {
     dte="$(date +"%A, %B %d - %I:%M")"
     echo -e " $dte"
 }
-dte
+
+while true; do
+    xsetroot -name "$(dte)"
+    sleep 1s
+done &
