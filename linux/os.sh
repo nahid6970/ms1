828424f0e2f3a51f82a2d4270c06f312757b00ac
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2025-05-08 03:36:40 +0600

    📅 May 08, 2025, 03:36 AM

--- linux/os.sh
+++ linux/os.sh
@@ -52,9 +52,11 @@ echo Set All configs
 mkdir -p "$HOME/.config/conky"
 mkdir -p "$HOME/.config/foot"
 mkdir -p "$HOME/.config/wofi"
-rsync -a --delete "~/ms1/config/foot/" "~/.config/foot/"
-rsync -a --delete "~/ms1/config/wofi/" "~/.config/wofi/"
-rsync -a --delete "~/ms1/linux/config/conky/" "~/.config/conky/"
+
+rsync -a --delete "$HOME/ms1/config/foot/" "$HOME/.config/foot/"
+rsync -a --delete "$HOME/ms1/config/wofi/" "$HOME/.config/wofi/"
+rsync -a --delete "$HOME/ms1/linux/config/conky/" "$HOME/.config/conky/"
+
 }
 
 Load_Scripts(){
