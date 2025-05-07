2c5814ba9745f29fc0fdb60d1a205b3e1a2c239f
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2025-05-07 11:06:03 +0600

    📅 May 07, 2025, 11:06 AM

--- archlinux/xmonad/xmonad.hs
+++ archlinux/xmonad/xmonad.hs
@@ -22,7 +22,7 @@ myClickJustFocuses = False
 
 -- Width of the window border in pixels.
 --
-myBorderWidth   = 10
+myBorderWidth   = 1
 
 -- modMask lets you specify which modkey you want to use. The default
 -- is mod1Mask ("left alt").  You may also consider using mod3Mask
@@ -240,8 +240,6 @@ myLogHook = return ()
 myStartupHook = do
         spawnOnce "nitrogen --restore &"
         spawnOnce "picom &"
-        -- spawnOnce "sh" ["~/.config/autostart.h"]
-
 
 ------------------------------------------------------------------------
 -- Now run xmonad with all the defaults we set up.
