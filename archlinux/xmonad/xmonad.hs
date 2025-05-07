295dd34d026302f2cb1a2e93753f29ffdc4b13c0
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2025-05-07 08:38:31 +0600

    📅 May 07, 2025, 08:38 AM

--- archlinux/xmonad/xmonad.hs
+++ archlinux/xmonad/xmonad.hs
@@ -22,7 +22,7 @@ myClickJustFocuses = False
 
 -- Width of the window border in pixels.
 --
-myBorderWidth   = 10
+myBorderWidth   = 1
 
 -- modMask lets you specify which modkey you want to use. The default
 -- is mod1Mask ("left alt").  You may also consider using mod3Mask
