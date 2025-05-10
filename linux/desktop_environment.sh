711dcb04459537480628c70ae1ee4dbdebf1e82e
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2025-05-11 00:14:17 +0600

    📅 May 11, 2025, 12:14 AM

--- linux/desktop_environment.sh
+++ linux/desktop_environment.sh
@@ -87,12 +87,12 @@ EOF
             ;;
         7)
             echo -e "${YELLOW}Installing qtile.${NC}"
-            sudo pacman -S --needed qtile python-pywlroots xorg-xwayland
+            sudo pacman -S --needed qtile python-pywlroots xorg-xwayland kitty dmenu picom network-manager-applet volumeicon
             rclone copy "$HOME/ms1/linux/config/.config/qtile" "$HOME/.config/qtile"
 
             # qtile start #!xorg/x11
             # qtile start -b wayland #!wayland
-            
+
             # sddm_setup
             # wallpaper
             ;;
