aa7840edb0dd11ee9a6f0b184dbfd85cc70511f4
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2025-05-14 12:18:07 +0600

    📅 May 14, 2025, 12:18 PM

--- linux/import/container.sh
+++ linux/import/container.sh
@@ -8,7 +8,8 @@ NC='\033[0m' # No Color
 echo -e "${GREEN}🔧 Choose setup option:${NC}"
 echo -e "1) Steam"
 echo -e "2) Lutris"
-read -rp "Enter your choice (1/2): " choice
+echo -e "3) Bottles"
+read -rp "Enter your choice (1/2/3): " choice
 
 lutris_cont() {
     clear
@@ -20,11 +21,17 @@ steam_cont() {
     sudo pacman -S --needed steam
 }
 
+bottles_cont() {
+    clear
+    sudo pacman -S --needed bottles 
+}
+
 
 
 case $choice in
     1) steam_cont ;;
     2) lutris_cont ;;
+    3) bottles_cont ;;
     *) echo "Invalid choice." ;;
 esac
 
