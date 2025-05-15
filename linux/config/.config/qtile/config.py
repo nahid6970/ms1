336036e9d38cb01c1071a0356fe7d49e2ed5110d
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2025-05-16 02:13:15 +0600

    📅 May 16, 2025, 02:13 AM

--- linux/config/.config/qtile/config.py
+++ linux/config/.config/qtile/config.py
@@ -380,7 +380,7 @@ def init_widgets_list():
         widget.Clock(
                 foreground=colors[8],
                 padding=8,
-                mouse_callbacks={'Button1': lambda: qtile.cmd_spawn('dbus-launch dunstify -t 5000 -u critical "$(date)"', shell=True)},
+                mouse_callbacks={'Button1': lambda: qtile.cmd_spawn('dbus-launch dunstify -t 5000 -u critical "$(date)"')},
                 format=" %I:%M %p",
                 ),
         widget.Systray(padding = 6),
