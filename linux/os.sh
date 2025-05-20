f72144bec2f68295c71c29651d4f8cdd3915c19c
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2025-05-20 09:10:50 +0600

    📅 May 20, 2025, 09:10 AM

--- linux/os.sh
+++ linux/os.sh
@@ -4,7 +4,6 @@
 for file in ~/ms1/linux/import/*.sh; do [[ -r "$file" ]] && source "$file" || echo "Error sourcing $file" >&2; done
 #! C:\ms1\linux\import\compositors.sh
 #! C:\ms1\linux\import\container.sh
-#! C:\ms1\linux\import\install_packages.sh
 #! C:\ms1\linux\import\sddm.sh
 #! C:\ms1\linux\import\StatusBar.sh
 #! C:\ms1\linux\import\store.sh
@@ -100,6 +99,25 @@ xorg(){
 wayland(){
     sudo pacman -S --needed wayland wayland-protocols wayland-utils xdg-desktop-portal xdg-desktop-portal-wlr wlroots libinput gtk3 qt5-wayland xorg-xwayland waybar wofi grim slurp wl-clipboard swaylock
 }
+
+# Function to install necessary packages using yay
+install_packages() {
+    clear
+    echo -e "${GREEN}Updating package database...${NC}"
+    sudo pacman -Sy --noconfirm
+    echo -e "${GREEN}Installing Necessary Packages...${NC}"
+    sudo pacman -S --needed \
+        bash bat chafa curl eza fastfetch fzf \
+        lsd lua-language-server neovim \
+        openssh python rclone sshpass wget \
+        which zoxide yazi zsh stow expac numlockx \
+        rsync ttf-jetbrains-mono-nerd ttf-jetbrains-mono \
+        thefuck feh screenfetch sed grep jq rofi conky \
+        htop firefox dunst mypy pcmanfm thunar thunar-archive-plugin thunar-volman \
+        vlc audacious \
+        foot starship 
+}
+
 
 rclone_decrypt() {
     # remove te ntfy file
