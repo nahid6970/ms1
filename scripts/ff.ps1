97f80e5fb22867b0938db189ea0fd9701768ac76
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2025-05-23 20:02:21 +0600

    📅 May 23, 2025, 08:02 PM 🚀 ff updated

--- scripts/ff.ps1
+++ scripts/ff.ps1
@@ -1,15 +1,32 @@
-# Directories to search
-$directories = @(
-    "C:\ms1",
-    "C:\ms2",
-    "C:\ms3",
-    "D:\",
-    "C:\Program Files\WindowsApps",
-    "C:\Users\nahid"
-) | Where-Object { $_ -ne "" }
+# Start with an empty array
+$directories = @()
+
+# Add directories (comment any line freely without breaking syntax)
+$directories += "C:\ms1"
+$directories += "C:\ms2"
+$directories += "C:\ms3"
+# $directories += "D:\"
+# $directories += "C:\Program Files\WindowsApps"
+# $directories += "C:\Users\nahid"
+$directories = $directories | Where-Object { $_ -ne "" -and $_ -ne $null }
+
 
 # Ignore list
 $ignoreList = @(".git", ".pyc")
+
+# Shortcut list text for F1 display
+$shortcutsText = @"
+Shortcuts available:
+  Enter   : Open selected file in VSCode
+  Ctrl-o  : Open file location in Explorer
+  Ctrl-c  : Copy full file path to clipboard
+  Ctrl-r  : Run file with PowerShell Start-Process
+  F1      : Show this shortcuts help window
+"@
+
+# Create a temp file with shortcuts text
+$tempShortcutFile = [System.IO.Path]::GetTempFileName()
+Set-Content -Path $tempShortcutFile -Value $shortcutsText -Encoding UTF8
 
 # Main function
 function SearchDirectoriesAndFiles {
@@ -25,7 +42,8 @@ function SearchDirectoriesAndFiles {
             --bind "enter:execute-silent(code {+1})" `
             --bind "ctrl-o:execute-silent(explorer.exe {+2})" `
             --bind "ctrl-c:execute-silent(cmd /c echo {+1} | clip)" `
-            --bind "ctrl-r:execute-silent(powershell -command Start-Process '{+1}')"
+            --bind "ctrl-r:execute-silent(powershell -command Start-Process '{+1}')" `
+            --bind "f1:execute-silent(cmd /c start cmd /k type `"$tempShortcutFile`" & pause)"
 }
 
 # Call main
