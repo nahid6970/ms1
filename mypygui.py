6b8c02aaf647aadc1d3c5cc2da926c298c955c6d
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2025-06-30 11:33:06 +0600

    📅 June 30, 2025, 11:33 AM

--- mypygui.py
+++ mypygui.py
@@ -607,8 +607,8 @@ ShadowFight3_lb.pack(side="left", padx=(0, 0), pady=(0, 0))
 # ShadowFight3_lb.bind("<Button-1>",lambda event=None:subprocess.Popen('cmd /c start C:\\ms1\\SH3\\SH3V2.py'))
 ShadowFight3_lb.bind( "<Button-1>", lambda event=None: run_command(r'python C:\ms1\scripts\shadowFight3\shadowFight3.py'))
 ShadowFight3_lb.bind("<Control-Button-1>",lambda event=None: run_command('code C:\\ms1\\scripts\\shadowFight3\\shadowFight3.py'))
-ShadowFight3_lb.bind( "<Button-3>", lambda event=None: run_command(r'python C:\ms1\scripts\CoC\CoC.py'))
-ShadowFight3_lb.bind("<Control-Button-3>",lambda event=None: run_command('code C:\\ms1\\scripts\\CoC\\CoC.py'))
+ShadowFight3_lb.bind( "<Button-3>", lambda event=None: run_command(r'python C:\ms1\scripts\CoC\clash_of_clan.py'))
+ShadowFight3_lb.bind("<Control-Button-3>",lambda event=None: run_command(r'code C:\ms1\scripts\CoC\clash_of_clan.py'))
 
 
 #! FFMPEG
