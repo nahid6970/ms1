d0db72bd656cce616d1756862770c588379dd9df
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2025-07-05 09:38:42 +0600

    📅 July 05, 2025, 09:38 AM 🚀 qtile

--- linux/config/.config/qtile/startup.sh
+++ linux/config/.config/qtile/startup.sh
@@ -2,12 +2,12 @@
 # numlockx on
 
 
-# # Start dbus session if not already running and export env variables
-# if [ -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
-#     eval $(dbus-launch --sh-syntax --exit-with-session)
-#     export DBUS_SESSION_BUS_ADDRESS
-#     export DBUS_SESSION_BUS_PID
-# fi
+# Start dbus session if not already running and export env variables
+if [ -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
+    eval $(dbus-launch --sh-syntax --exit-with-session)
+    export DBUS_SESSION_BUS_ADDRESS
+    export DBUS_SESSION_BUS_PID
+fi
 
 # # Add other startup commands below if needed
 # # e.g., picom, nm-applet, etc.
