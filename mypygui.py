f298ee2e7b638a292badf46778bc8b4c057a6fa7
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2025-07-06 20:16:05 +0600

    📅 July 06, 2025, 08:16 PM

--- mypygui.py
+++ mypygui.py
@@ -520,8 +520,8 @@ Tools_bt.bind("<Control-Button-1>",lambda event:subprocess.Popen(["cmd /c code C
 
 Startup_bt=CTkLabel(ROOT1, text="\uf4cc", bg_color="#1d2027",text_color="#10b153", anchor="w",font=("JetBrainsMono NFP",25,"bold"))
 Startup_bt.pack(side="left",padx=(2,0),pady=(1,0))
-Startup_bt.bind("<Button-1>",lambda event:subprocess.Popen(["cmd /c C:\\ms1\\startup.py"], shell=True))
-Startup_bt.bind("<Control-Button-1>",lambda event:subprocess.Popen(["cmd /c code C:\\ms1\\startup.py"], shell=True))
+Startup_bt.bind("<Button-1>",lambda event:subprocess.Popen([r"cmd /c C:\ms1\startup\startup.py"], shell=True))
+Startup_bt.bind("<Control-Button-1>",lambda event:subprocess.Popen([r"cmd /c code C:\ms1\startup\startup.py"], shell=True))
 
 
 # ProcessPRLS_bt=CTkLabel(ROOT1, text="\uf4bc", bg_color="#1d2027",text_color="#f04410", anchor="w",font=("JetBrainsMono NFP",25,"bold"))
