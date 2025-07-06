3fea6072d56b1b5fafd4b4842d58508c5d5cbbdf
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2025-07-06 10:13:05 +0600

    📅 July 06, 2025, 10:13 AM

--- Testing/menu.py
+++ Testing/menu.py
@@ -21,7 +21,7 @@ class ArchUtil:
         
         # Initialize colors
         curses.start_color()
-        curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)   # Selected
+        curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)   # Selected
         curses.init_pair(2, curses.COLOR_CYAN, curses.COLOR_BLACK)   # Title
         curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_BLACK)  # Success
         curses.init_pair(4, curses.COLOR_RED, curses.COLOR_BLACK)    # Error
