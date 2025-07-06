9984086c261cd4b84826bd90613e6402979d02d1
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2025-07-06 19:34:31 +0600

    📅 July 06, 2025, 07:34 PM

--- myhome/app.py
+++ myhome/app.py
@@ -70,5 +70,5 @@ def delete_link(link_id):
         return jsonify({'message': 'Link deleted successfully', 'deleted_link': deleted_link})
     return jsonify({'message': 'Link not found'}), 404
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
