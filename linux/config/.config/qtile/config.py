7deee35351ba951aa1eccd55ec7d7427834f0200
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2025-07-08 14:06:48 +0600

    📅 July 08, 2025, 02:06 PM

--- linux/config/.config/qtile/config.py
+++ linux/config/.config/qtile/config.py
@@ -393,7 +393,7 @@ def init_widgets_list():
                  mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn('notify-disk')},
                  partition = '/',
                  #format = '[{p}] {uf}{m} ({r:.0f}%)',
-                 format = '{uf}{m} freeee',
+                 format = '{uf}{m} free',
                  fmt = '  Disk: {}',
                  visible_on_warn = False,
                  ),
