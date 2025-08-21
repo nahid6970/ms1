c290dd1de68c5af97b6fdbfc7955c5c74581a235
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2025-08-21 14:02:47 +0600

    Auto-commit

---
+++ test_project/rust/gemini_rust_project/target/debug/deps/gemini_rust_project.d
@@ -0,0 +1,5 @@
+C:\Users\nahid\ms\ms1\test_project\rust\gemini_rust_project\target\debug\deps\gemini_rust_project.d: src\main.rs
+
+C:\Users\nahid\ms\ms1\test_project\rust\gemini_rust_project\target\debug\deps\gemini_rust_project.exe: src\main.rs
+
+src\main.rs:
