a5b1b255f9badba4d14ab64f27eee6e6390b9ad7
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2025-10-01 23:43:00 +0600

    yasb

--- asset/yasb_reborn/x_scoop_count_package.bat
+++ asset/yasb_reborn/x_scoop_count_package.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
 
-for /f %%i in ('scoop status ^| findstr /v "Scoop WARN Name ----" ^| findstr /r "." ^| find /c /v ""') do set count=%%i
+for /f %%i in ('scoop status ^| findstr /v "Everything Scoop WARN Name ----" ^| findstr /r "." ^| find /c /v ""') do set count=%%i
 
 if %count% NEQ 0 (
     echo {"count":%count%}
