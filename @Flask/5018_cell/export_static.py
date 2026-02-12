09b7d9c2c065f3e25176bf2b9c5d52cd4f6a24a3
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2026-02-12 13:03:27 +0600

    fixed export table list

--- @Flask/5018_cell/export_static.py
+++ @Flask/5018_cell/export_static.py
@@ -2363,20 +2363,24 @@ def generate_static_html(data, custom_syntaxes):
 
             // List support in cells (-, --, ---, ----, -----)
             if (formatted.indexOf('- ') !== -1 || formatted.indexOf('. ') !== -1) {
-                var lines = formatted.split(new RegExp('<br>|\\\\n', 'g'));
+                var newlineChar = String.fromCharCode(10);
+                var lines = formatted.replace(/<br>/g, newlineChar).split(newlineChar);
                 var recovered = [];
                 for (var i = 0; i < lines.length; i++) {
                     var line = lines[i];
                     var trimmed = line.trim();
-                    var listMatch = trimmed.match(/^(\\\\-{1,5})\\\\s/);
+                    
+                    // Bullet lists
+                    var listMatch = trimmed.match(/^(\-{1,5})\s/);
                     if (listMatch) {
                         var markerLength = listMatch[1].length;
-                        var icons = ['\\\\u2022', '\\\\u25e6', '\\\\u25aa', '\\\\u25b8', '\\\\u2212'];
-                        var icon = icons[markerLength - 1] || '\\\\u2022';
-                        var content = line.replace(/^\\\\s*(\\\\-{1,5})\\\\s/, '');
+                        var icons = ['•', '◦', '▪', '▸', '−'];
+                        var icon = icons[markerLength - 1] || '•';
+                        var content = line.replace(/^\s*(\-{1,5})\s/, '');
                         recovered.push('<span style="display: inline-block; width: 100%; box-sizing: border-box; padding-left: ' + markerLength + 'em; text-indent: -1em; white-space: pre-wrap;"><span style="display: inline-block; width: 1em; text-indent: 0;">' + icon + '</span>' + content + '</span>');
                     } else {
-                        var numMatch = trimmed.match(/^(\\\\d+\\\\.\\\\s)(.+)$/);
+                        // Numbered lists
+                        var numMatch = trimmed.match(/^(\d+\.\s)(.+)$/);
                         if (numMatch) {
                             var number = numMatch[1];
                             var contentPart = numMatch[2];
