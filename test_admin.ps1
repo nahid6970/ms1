44bf381d5ea4292dcadfe8741b129ebf880be344
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2026-04-08 09:32:23 +0600

    fixed bugs

--- test_admin.ps1
+++ test_admin.ps1
@@ -1,3 +1,7 @@
 $isAdmin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-Write-Host "Running as Admin: $isAdmin" -ForegroundColor ($isAdmin ? "Green" : "Red")
+if ($isAdmin) {
+    Write-Host "Running as Admin: YES" -ForegroundColor Green
+} else {
+    Write-Host "Running as Admin: NO" -ForegroundColor Red
+}
 Read-Host "Press Enter to exit"
