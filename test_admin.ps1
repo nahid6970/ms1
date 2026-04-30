b7bead8f36b325574b897318d0044ba2ca387e45
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2026-04-30 13:55:43 +0600

    removed trash

--- test_admin.ps1
+++
@@ -1,7 +0,0 @@
-$isAdmin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-if ($isAdmin) {
-    Write-Host "Running as Admin: YES" -ForegroundColor Green
-} else {
-    Write-Host "Running as Admin: NO" -ForegroundColor Red
-}
-Read-Host "Press Enter to exit"
